ee7a8b7e93c2078028415c7d452417ec9997e1c5
Souvik Basu <souvikbasu@gmail.com>
2019-08-24 23:44:27 -0700

    add city thumbnail component and city page

---
+++ src/components/CityThumbnail.vue
@@ -0,0 +1,97 @@
+<template>
+  <!-- div for searching city -->
+  <router-link :to="'/city/' + city._id">
+    <div class="thumbnail">
+      <img
+        class="photo"
+        src="/images/city-thumb.jpg"
+      >
+      <div class="details">
+        <div>{{ city.name }}, {{ city.country }}</div>
+        <div class="text-sm">
+          <span class="left half-width">{{ city.devCount }} devs</span>
+          <span class="half-width">{{ city.designersCount }} designers</span>
+        </div>
+        <div class="text-xs tiny-details">
+          <tiny-key-value
+            label="React"
+            value="60"
+            class="half-width pr-2"
+          /><tiny-key-value
+            label="Web"
+            value="60"
+            class="half-width"
+          />
+        </div>
+        <div class="text-xs tiny-details">
+          <tiny-key-value
+            label="Angular"
+            value="160"
+            class="half-width pr-2"
+          /><tiny-key-value
+            label="Android"
+            value="260"
+            class="half-width"
+          />
+        </div>
+        <div class="text-xs tiny-details">
+          <tiny-key-value
+            label="Vue"
+            value="6000"
+            class="half-width pr-2"
+          /><tiny-key-value
+            label="iOS"
+            value="1060"
+            class="half-width"
+          />
+        </div>
+        <div class="text-xs">
+          ReactFoo 2019, JSFoo 2019
+        </div>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+import TinyKeyValue from "@/components/TinyKeyValue";
+
+export default {
+  components: {
+    TinyKeyValue
+  },
+
+  methods: {}
+};
+</script>
+
+<style scoped>
+/* style for city thumbnail */
+.thumbnail {
+  border: solid #aada18 2px;
+  height: 12rem;
+  width: 12rem;
+  min-width: 12rem;
+  color: #2c3e50;
+  text-decoration: none;
+}
+
+.thumbnail .photo {
+  background-color: #aada18;
+  height: 5rem;
+  width: 100%;
+}
+
+.details {
+  padding: 0.5rem;
+  text-align: left;
+}
+
+.half-width {
+  width: 50%;
+}
+
+.pr-2 {
+  padding-right: 10px;
+}
+</style>
