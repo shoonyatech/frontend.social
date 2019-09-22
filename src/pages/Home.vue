c6619be81e0285f958baf18f14a351fd288346b8
Souvik Basu <souvikbasu@gmail.com>
2019-09-21 20:47:58 -0700

    add to home page message

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="home">
-    Hangout with Frontend Developers and Designers..
+  <div>
+    <div class="home">
+      Hangout with Frontend Developers and Designers..
+    </div>
+
+    <div class="text">
+      Meet fellow developers and designers in your city meetups, learn from
+      online courses and find jobs matching your skills
+    </div>
+
+    <!-- developer box div -->
+    <div class="join-box">
+      <div class="msg">
+        I am a developer/designer
+      </div>
+      <a
+        href="/signin"
+        class="join-button"
+      >Join for Free</a>
+    </div>
   </div>
 </template>
 
@@ -9,3 +27,25 @@ export default {
   name: "Home"
 };
 </script>
+
+<style scoped>
+.join-box {
+  border: 2px solid #aada20;
+  max-width: 417px;
+  height: 200px;
+  margin: 0 auto;
+}
+
+.join-box .msg {
+  margin-top: 20px;
+}
+
+.join-button {
+  background: #aada20;
+  color: #fff;
+  border: none;
+  padding: 8px;
+  margin-bottom: 20px;
+  margin-top: 30px;
+}
+</style>
