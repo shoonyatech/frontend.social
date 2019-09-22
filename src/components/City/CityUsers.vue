5ee65a45c2373d4001700042b57c1eefb724eb5b
Souvik Basu <souvikbasu@gmail.com>
2019-09-21 23:21:00 -0700

    add placeholder components for showing developers and designers in city

---
+++ src/components/City/CityUsers.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="city-users">
+    <div>{{ userGroup }} from {{ city }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+
+  props: {
+    userGroup: { type: String, required: true },
+    city: { type: String, required: true },
+    users: { type: Array, required: true }
+  },
+
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
