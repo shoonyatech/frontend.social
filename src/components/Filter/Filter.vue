a53aa35c345d655deefc868c816df2fd9876bd2c
Sanjay Singh <ssingh@falabella.cl>
2019-09-28 23:47:44 +0530

    add filter component

---
+++ src/components/Filter/Filter.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="filter">
+    <input
+      :id="id"
+      :type="type"
+      :value="value"
+      :name="name"
+      :checked="isSelected"
+      @input="onClick(id)"
+    >
+    <label :for="id">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    label: {
+      type: String
+    },
+    name: {
+      type: String,
+      default: ""
+    },
+    onClick: {
+      type: Function,
+      default: () => {}
+    },
+    type: {
+      type: String,
+      default: "checkbox"
+    },
+
+    value: {
+      type: String,
+      default: ""
+    },
+    id: {
+      type: String,
+      default: ""
+    },
+    isSelected: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+.filter > label {
+  margin-left: 5px;
+}
+</style>
