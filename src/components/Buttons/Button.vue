bd1c54ac198dfea55278d01283998d3b9989bd73
Sanjay Singh <ssingh@falabella.cl>
2019-09-28 22:34:23 +0530

    add Button component

---
+++ src/components/Buttons/Button.vue
@@ -0,0 +1,43 @@
+<template>
+  <button
+    :class="[type]"
+    @click="onClick"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    label: {
+      type: String,
+      default: ""
+    },
+    onClick: {
+      type: Function,
+      default: () => {}
+    },
+    type: {
+      type: String,
+      default: "primary"
+    }
+  }
+};
+</script>
+
+<style scoped>
+button {
+  padding: 5px;
+  width: 100%;
+  height: 100%;
+  border: none;
+  font-size: 19px;
+  cursor: pointer;
+}
+button.primary {
+  background-color: #aada18;
+  color: white;
+}
+</style>
