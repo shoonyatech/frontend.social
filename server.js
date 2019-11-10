3fceec7838342b63b7397b2ce8982af2c620ff70
Sanjay Singh <ssingh@falabella.cl>
2019-11-10 13:08:45 +0530

    remove passsport

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require("express"),
   path = require("path"),
   app = express();
-
+require("dotenv").config();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist"));
 app.use(logUrl);
 
+app.use(logUrl);
 function logUrl(req, res, next) {
   console.log(req.url);
   return next();
