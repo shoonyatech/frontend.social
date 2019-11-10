90021acf3a45bece57c28aa3f39ee182ccfc6403
Souvik Basu <souvikbasu@gmail.com>
2019-11-09 19:32:54 -0800

    show social links in profile page

---
+++ src/components/common/KeyValue.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="host">
+    <span class="label">{{ label }}</span>
+    <span class="value">{{ value }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    label: {
+      type: String,
+      default: ""
+    },
+    value: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style scoped>
+.host {
+  display: inline-block;
+  width: 100%;
+  padding-left: 10px;
+  text-align: left;
+}
+
+.label {
+  color: #aada18;
+  width: 150px;
+  display: inline-block;
+}
+
+.value {
+  display: inline-block;
+}
+</style>
