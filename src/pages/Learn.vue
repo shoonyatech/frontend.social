82267ef54b224b5b49efecdbc66c78b61e49c105
Souvik Basu <souvikbasu@gmail.com>
2019-11-15 08:23:53 -0800

    add the list of latest articles in learn page

--- src/pages/Learn.vue
+++ src/pages/Learn.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="learn">
-    <h1>You are on Learn page</h1>
+    <b-container>
+      <b-row>
+        <h1>Latest articles on Frontend</h1>
+        <div class="articles">
+          <a
+            v-for="article in articles"
+            :key="article.url"
+            :href="article.url"
+          >{{ article.title }}</a>
+        </div>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Learn"
+  name: "Learn",
+  components: {},
+  data() {
+    return {
+      articles: []
+    };
+  },
+  created() {},
+  methods: {}
 };
 </script>
+
+
+<style scoped>
+.articles {
+  margin: 0 10px;
+}
+</style>
