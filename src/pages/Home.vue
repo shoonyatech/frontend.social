a299024a1baf5bd05084a70e5dde1f28999fa1c1
Souvik Basu <souvikbasu@gmail.com>
2019-11-17 15:13:20 -0800

    update home page layout to be grid based

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,26 +1,21 @@
 <template>
   <b-container>
     <b-row>
-      <b-col md="12">
+      <b-col md="9">
+        <h1>Find out What's new!</h1>
         <div class="home">
-          Hangout with Frontend Developers and Designers..
-        </div>
-        <div class="text">
-          Meet fellow developers and designers in your city meetups, learn from
-          online courses and find jobs matching your skills
+          Hangout with Frontend Developers and Designers. Meet fellow developers
+          and designers in your city meetups, learn from online courses and find
+          jobs matching your skills!!
         </div>
       </b-col>
-    </b-row>
-    <b-row>
-      <b-col md="12">
+      <b-col md="3">
         <div class="join-box">
-          <div class="msg">
-            I am a developer/designer
-          </div>
-          <a
-            href="/signin"
-            class="join-button"
-          >Join for Free</a>
+          <span class="join-label">Join for Free</span>
+          <facebook-login
+            class="social-button"
+            app-id="311503802832589"
+          />
         </div>
       </b-col>
     </b-row>
@@ -28,12 +23,30 @@
 </template>
 
 <script>
+import FacebookLogin from "@/components/Signin/FacebookLogin.vue";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    FacebookLogin
+  },
+  data() {
+    return {
+      isSignedIn: false
+    };
+  },
+  created() {
+    this.isSignedIn = false;
+  },
+  methods: {}
 };
 </script>
 
 <style scoped>
+.home {
+  text-align: left;
+}
+
 .join-box {
   border: 2px solid #aada20;
   max-width: 417px;
@@ -45,12 +58,18 @@ export default {
   margin-top: 20px;
 }
 
-.join-button {
+.join-label {
   background: #aada20;
   color: #fff;
   border: none;
-  padding: 8px;
+  padding: 4px;
   margin-bottom: 20px;
-  margin-top: 30px;
+  width: 100%;
+  display: inline-block;
+}
+
+.social-button {
+  font-size: 0.75rem;
+  padding: 0 !important;
 }
 </style>
