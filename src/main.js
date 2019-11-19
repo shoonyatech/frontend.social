c3852d74fbffbc1fa2c0db44c458fc5d296b3005
Souvik Basu <souvikbasu@gmail.com>
2019-11-19 08:21:38 -0800

    add vue-authenticate for social logins

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
+import VueAxios from "vue-axios";
+import axios from "axios";
+import VueAuthenticate from "vue-authenticate";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
@@ -10,6 +13,32 @@ import router from "./router";
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
+Vue.use(VueAxios, axios);
+Vue.use(VueAuthenticate, {
+  tokenName: "access_token",
+  baseUrl: "http://localhost:3000/",
+  storageType: "cookieStorage",
+  providers: {
+    facebook: {
+      name: "facebook",
+      url: "fb-signin",
+      authorizationEndpoint: "https://www.facebook.com/v5.0/dialog/oauth",
+      redirectUri: "http://localhost:8080/signin/fb",
+      requiredUrlParams: ["display", "scope"],
+      scope: ["email"],
+      scopeDelimiter: ",",
+      display: "popup",
+      oauthType: "2.0",
+      popupOptions: { width: 580, height: 400 },
+      clientId: "",
+      clientSecret: ""
+    },
+    github: {
+      clientId: "",
+      redirectUri: "http://localhost:8080/auth/callback" // Your client app URL
+    }
+  }
+});
 
 new Vue({
   router,
