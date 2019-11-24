68c284b398bb5bbfd3f5f948a78889aa54181b9b
Souvik Basu <souvikbasu@gmail.com>
2019-11-23 16:50:58 -0800

    remove calls to get user porfile from HandleFBResponse component and put in Home

--- src/pages/HandleFBResponse.vue
+++ src/pages/HandleFBResponse.vue
@@ -5,28 +5,12 @@
 </template>
 
 <script>
-import userService from "@/services/user.service";
-
 export default {
   components: {},
   data() {
     return {};
   },
-  created() {
-    const queryParams = this.$route.hash.split("&");
-    const accessTokenParam = queryParams.find(q =>
-      q.startsWith("#access_token=")
-    );
-    const accessToken = accessTokenParam.substr(
-      accessTokenParam.indexOf("=") + 1
-    );
-    userService.fbSignin(accessToken).then(res => {
-      const user = res.data;
-      localStorage.setItem("profile", JSON.stringify(user));
-      this.$store.commit("signInUser", user);
-      this.$router.push("/me");
-    });
-  },
+  created() {},
   methods: {}
 };
 </script>
