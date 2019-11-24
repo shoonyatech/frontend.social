5859e7a052221c4bf02590cb65a7f9fac2e26860
Souvik Basu <souvikbasu@gmail.com>
2019-11-23 16:48:53 -0800

    add empty keys file

---
+++ src/config/keys.js
@@ -0,0 +1,22 @@
+export default {
+  facebook: {
+    clientId: "",
+    clientSecret: ""
+  },
+  github: {
+    clientId: "",
+    clientSecret: ""
+  },
+  twitter: {
+    clientId: "",
+    clientSecret: ""
+  },
+  bitbucket: {
+    clientId: "",
+    clientSecret: ""
+  },
+  linkedin: {
+    clientId: "",
+    clientSecret: ""
+  }
+};
