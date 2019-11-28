a858b96b1063db8be737f8dd2cdc1ec4808f0106
Souvik Basu <souvikbasu@gmail.com>
2019-11-27 18:53:30 -0800

    refactor LatestArticles as a component and reuse in Learn and Home pages

---
+++ src/components/Learn/LatestArticles.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="host">
+    <h1>Latest articles on Frontend</h1>
+    <div class="articles">
+      <a
+        v-for="article in articles"
+        :key="article.url"
+        :href="article.url"
+      >{{
+        article.title
+      }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      articles: []
+    };
+  }
+};
+</script>
+
+<style scoped>
+.host {
+  width: 100%;
+}
+
+.articles {
+  margin: 0 10px;
+}
+</style>
