a9eddd306a82aa8ccfbdc5166b6ce87ab329449a
Souvik Basu <souvikbasu@gmail.com>
2019-11-29 22:05:55 -0800

    show article details in LatestArticles component

--- src/components/Learn/LatestArticles.vue
+++ src/components/Learn/LatestArticles.vue
@@ -2,13 +2,20 @@
   <div class="host">
     <h1>Latest articles on Frontend</h1>
     <div class="articles">
-      <a
+      <div
         v-for="article in articles"
         :key="article.url"
-        :href="article.url"
-      >{{
-        article.title
-      }}</a>
+        class="article"
+      >
+        <div>
+          <a :href="article.url">{{ article.title }}</a>
+          <span> by {{ article.author }}</span>
+        </div>
+        <div>{{ article.details }}</div>
+        <div class="courtesy">
+          courtesy {{ article.courtesy }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,6 +36,16 @@ export default {
 }
 
 .articles {
-  margin: 0 10px;
+  margin: 20px 10px;
+  text-align: left;
+}
+
+.article {
+  margin-bottom: 40px;
+}
+
+.courtesy {
+  font-size: 0.75rem;
+  text-align: right;
 }
 </style>
