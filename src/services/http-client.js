563c3a87b9896703ec446c108a35927f0f307efc
Souvik Basu <souvikbasu@gmail.com>
2019-12-06 18:05:01 -0800

    refactor http-client to take out common functionality of setting auth header

--- src/services/http-client.js
+++ src/services/http-client.js
@@ -9,54 +9,33 @@ const httpClient = () => {
 
 export default {
   get(url) {
-    const profile = JSON.parse(localStorage.getItem("profile"));
-    let options = {};
-    if (profile && profile.authToken) {
-      options.headers = {
-        Authorization: `Bearer ${profile.authToken}`
-      };
-    }
-
-    return httpClient().get(url, {
-      headers: {
-        Authorization: `Bearer ${profile.authToken}`
-      }
-    });
+    let options = getOptions();
+    return httpClient().get(url, options);
   },
+
   post(url, data) {
-    const profile = JSON.parse(localStorage.getItem("profile"));
-    if (profile && profile.authToken) {
-      return httpClient().post(url, data, {
-        headers: {
-          Authorization: `Bearer ${profile.authToken}`
-        }
-      });
-    }
-
-    console.error("Auth Token not found");
+    let options = getOptions();
+    return httpClient().post(url, data, options);
   },
+
   put(url, data) {
-    const profile = JSON.parse(localStorage.getItem("profile"));
-    if (profile && profile.authToken) {
-      return httpClient().put(url, data, {
-        headers: {
-          Authorization: `Bearer ${profile.authToken}`
-        }
-      });
-    }
-
-    console.error("Auth Token not found");
+    let options = getOptions();
+    return httpClient().put(url, data, options);
   },
-  delete(url, data) {
-    const profile = JSON.parse(localStorage.getItem("profile"));
-    if (profile && profile.authToken) {
-      return httpClient().delete(url, data, {
-        headers: {
-          Authorization: `Bearer ${profile.authToken}`
-        }
-      });
-    }
 
-    console.error("Auth Token not found");
+  delete(url, data) {
+    let options = getOptions();
+    return httpClient().delete(url, data, options);
   }
 };
+
+function getOptions() {
+  const profile = JSON.parse(localStorage.getItem("profile"));
+  let options = {};
+  if (profile && profile.authToken) {
+    options.headers = {
+      Authorization: `Bearer ${profile.authToken}`
+    };
+  }
+  return options;
+}
