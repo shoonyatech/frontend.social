00382eeef924121ce35cf6168b10e379009c42e4
Souvik Basu <souvikbasu@gmail.com>
2019-12-11 18:38:02 -0800

    add terms of usage page content

--- src/pages/TermsOfUse.vue
+++ src/pages/TermsOfUse.vue
@@ -2,9 +2,207 @@
   <b-container>
     <b-row>
       <b-col md="12">
-        <h1>Web Site Terms and Conditions of Use</h1>
-        <div />
-        <LatestArticles />
+        <h1 class="page-heading">
+          Web Site Terms and Conditions of Use
+        </h1>
+        <div class="terms">
+          <h2 class="term">
+            1. Terms
+          </h2>
+          <div>
+            By accessing this web site, you are agreeing to be bound by these
+            web site Terms and Conditions of Use, our Privacy Policy, all
+            applicable laws and regulations, and agree that you are responsible
+            for compliance with any applicable local laws. If you do not agree
+            with any of these terms, you are prohibited from using or accessing
+            this site. The materials contained in this web site are protected by
+            applicable copyright and trade mark law.
+          </div>
+          <h2 class="term">
+            2. Use License
+          </h2>
+          <div>
+            <ol type="a">
+              <li>
+                Permission is granted to temporarily download one copy of the
+                materials (information or software) on Frontend Social's web
+                site for personal, non-commercial transitory viewing only. This
+                is the grant of a license, not a transfer of title, and under
+                this license you may not:
+                <ol type="i">
+                  <li>
+                    modify or copy the materials;
+                  </li>
+                  <li>
+                    use the materials for any commercial purpose, or for any
+                    public display (commercial or non-commercial);
+                  </li>
+                  <li>
+                    attempt to decompile or reverse engineer any software
+                    contained on Frontend Social's web site;
+                  </li>
+                  <li>
+                    remove any copyright or other proprietary notations from the
+                    materials; or
+                  </li>
+                  <li>
+                    transfer the materials to another person or "mirror" the
+                    materials on any other server.
+                  </li>
+                </ol>
+              </li>
+              <li>
+                This license shall automatically terminate if you violate any of
+                these restrictions and may be terminated by Frontend Social at
+                any time. Upon terminating your viewing of these materials or
+                upon the termination of this license, you must destroy any
+                downloaded materials in your possession whether in electronic or
+                printed format.
+              </li>
+            </ol>
+          </div>
+          <h2 class="term">
+            3. Disclaimer
+          </h2>
+          <div>
+            The materials on Frontend Social's web site are provided "as is".
+            Frontend Social makes no warranties, expressed or implied, and
+            hereby disclaims and negates all other warranties, including without
+            limitation, implied warranties or conditions of merchantability,
+            fitness for a particular purpose, or non-infringement of
+            intellectual property or other violation of rights. Further,
+            Frontend Social does not warrant or make any representations
+            concerning the accuracy, likely results, or reliability of the use
+            of the materials on its Internet web site or otherwise relating to
+            such materials or on any sites linked to this site.
+          </div>
+          <h2 class="term">
+            4. Limitations
+          </h2>
+          <div>
+            In no event shall Frontend Social or its suppliers be liable for any
+            damages (including, without limitation, damages for loss of data or
+            profit, or due to business interruption,) arising out of the use or
+            inability to use the materials on Frontend Social's Internet site,
+            even if Frontend Social or an authorized representative has been
+            notified orally or in writing of the possibility of such damage.
+            Because some jurisdictions do not allow limitations on implied
+            warranties, or limitations of liability for consequential or
+            incidental damages, these limitations may not apply to you.
+          </div>
+          <h2 class="term">
+            5. Revisions and Errata
+          </h2>
+          <div>
+            The materials appearing on Frontend Social's web site could include
+            technical, typographical, or photographic errors. Frontend Social
+            does not warrant that any of the materials on its web site are
+            accurate, complete, or current. Frontend Social may make changes to
+            the materials contained on its web site at any time without notice.
+            Frontend Social does not, however, make any commitment to update the
+            materials.
+          </div>
+          <h2 class="term">
+            6. Links
+          </h2>
+          <div>
+            Frontend Social has not reviewed all of the sites linked to its
+            Internet web site and is not responsible for the contents of any
+            such linked site. The inclusion of any link does not imply
+            endorsement by Frontend Social of the site. Use of any such linked
+            web site is at the user's own risk.
+          </div>
+          <h2 class="term">
+            7. Copyright / Takedown
+          </h2>
+          <div>
+            Users agree and certify that they have rights to share all content
+            that they post on frontend.social — including, but not limited to,
+            information posted in articles, discussions, and comments. This rule
+            applies to prose, code snippets, collections of links, etc.
+            Regardless of citation, users may not post copy and pasted content
+            that does not belong to them. Users assume all risk for the content
+            they post, including someone else's reliance on its accuracy, claims
+            relating to intellectual property, or other legal rights. If you
+            believe that a user has plagiarized content, misrepresented their
+            identity, misappropriated work, or otherwise run afoul of DMCA
+            regulations, please email contact@shoonya.ca. Frontend Social may
+            remove any content users post for any reason.
+          </div>
+          <h2 class="term">
+            8. Site Terms of Use Modifications
+          </h2>
+          <div>
+            Frontend Social may revise these terms of use for its web site at
+            any time without notice. By using this web site you are agreeing to
+            be bound by the then current version of these Terms and Conditions
+            of Use.
+          </div>
+          <h2 class="term">
+            9. Frontend Social Trademarks and Logos Policy
+          </h2>
+          <div>
+            All uses of the Frontend Social logo, Frontend Social badges,
+            Shoonya Technologies Ltd. logo, brand slogans, iconography, and the
+            like, may only be used with express permission from Frontend Social.
+            Frontend Social reserves all rights, even if certain assets are
+            included in Frontend Social open source projects. Please contact
+            contact@shoonya with any questions or to request permission.
+          </div>
+          <h2 class="term">
+            10. Reserved Names
+          </h2>
+          <div>
+            Frontend Social has the right to maintain a list of reserved names
+            which will not be made publicly available. These reserved names may
+            be set aside for purposes of proactive trademark protection,
+            avoiding user confusion, security measures, or any other reason (or
+            no reason). Additionally, Frontend Social reserves the right to
+            change any already-claimed name at its sole discretion. In such
+            cases, Frontend Social will make reasonable effort to find a
+            suitable alternative and assist with any transition-related
+            concerns.
+          </div>
+          <h2 class="term">
+            11. Content Policy
+          </h2>
+          <div>
+            The following policy applies to comments, articles, and all other
+            works shared on the Frontend Social platform:
+            <ol type="a">
+              <li>
+                Users must make a good-faith effort to share content that is
+                on-topic, of high-quality, and is not designed primarily for the
+                purposes of promotion or creating backlinks.
+              </li>
+              <li>
+                Posts must contain substantial content — they may not merely
+                reference an external link that contains the full post.
+              </li>
+              <li>
+                If a post contains affiliate links, that fact must be clearly
+                disclosed. For instance, with language such as: “This post
+                includes affiliate links; I may receive compensation if you
+                purchase products or services from the different links provided
+                in this article.”
+              </li>
+            </ol>
+            Frontend Social reserves the right to remove any content that it
+            deems to be in violation of this policy at its sole discretion.
+            Additionally, Frontend Social reserves the right to restrict any
+            user’s ability to participate on the platform at its sole
+            discretion.
+          </div>
+          <h2 class="term">
+            12. Governing Law
+          </h2>
+          <div>
+            Any claim relating to Frontend Social's web site shall be governed
+            by the laws of the Province of British Columbia, Canada without
+            regard to its conflict of law provisions. General Terms and
+            Conditions applicable to Use of a Web Site.
+          </div>
+        </div>
       </b-col>
     </b-row>
   </b-container>
@@ -15,3 +213,13 @@ export default {
   name: "TermsOfUse"
 };
 </script>
+
+<style scoped lang="scss">
+.terms {
+  margin-bottom: 2rem;
+}
+
+.term {
+  margin-top: 2rem;
+}
+</style>
