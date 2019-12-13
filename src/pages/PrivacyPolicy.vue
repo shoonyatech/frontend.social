80d7ec43cb45fb34c2485a533187826740661c2d
Souvik Basu <souvikbasu@gmail.com>
2019-12-12 22:08:08 -0800

    fill up Privacy Policy page

--- src/pages/PrivacyPolicy.vue
+++ src/pages/PrivacyPolicy.vue
@@ -2,9 +2,345 @@
   <b-container>
     <b-row>
       <b-col md="12">
-        <h1>Privacy Policy</h1>
-        <div class="left" />
-        <LatestArticles />
+        <h1 class="page-heading">
+          Privacy Policy
+        </h1>
+        <div class="terms">
+          <div>
+            Effective May 25, 2018
+          </div>
+          This policy covers Shoonya Technologies Ltd. ("Shoonya" or "Company")
+          and https://www.frontend.social website("Frontend Social" or
+          "Website").
+
+          <h2 class="term">
+            What information Frontend Social collects and Why
+          </h2>
+          <div>
+            <p>
+              In order to give you the best possible experience using Frontend
+              Social, we collect information from your interactions with our
+              network. We use common internet technologies, such as cookies and
+              web server logs. We collect this basic information from everybody,
+              whether they have an account or not.
+            </p>
+            <div>
+              The information we collect about all visitors to our website
+              includes:
+              <ul>
+                <li>The visitor’s browser type</li>
+                <li>Referring site</li>
+                <li>The date and time of each visitor request</li>
+                <li>
+                  We also collect potentially personally-identifying information
+                  like Internet Protocol (IP) addresses.
+                </li>
+              </ul>
+            </div>
+            <div>
+              We use this information to:
+              <ul>
+                <li>
+                  Provide, test, improve, promote and personalize Frontend
+                  Social Services
+                </li>
+                <li>Fight spam and other forms of abuse</li>
+                <li>
+                  Generate aggregate, non-identifying information about how
+                  people use Frontend Social Services
+                </li>
+              </ul>
+            </div>
+            <div>
+              In order for you to create an account on Frontend Social and use
+              our Services, we need to collect and process certain information.
+              Depending on your use of the Services, that may include:
+              <ul>
+                <li>
+                  Communications you send to us (for example, when you ask for
+                  support, send us questions or comments, or report a problem);
+                </li>
+                <li>
+                  Information that you submit on or to Frontend Social in the
+                  form of reactions, comments, or messages to other users;
+                </li>
+                <li>
+                  The email address associated with your Twitter account, if you
+                  choose to sign up using your Twitter credentials. Frontend
+                  Social will also request permission to access additional
+                  information (these permissions are governed by Twitter’s
+                  privacy policies and can be managed through your Twitter
+                  privacy settings). We never post anything to your Twitter
+                  without your permission.
+                </li>
+                <li>
+                  The email address associated with your GitHub account, if you
+                  choose to sign up using your Github credentials. Frontend
+                  Social will also request permission to access additional
+                  information (these permissions are governed by GitHub’s
+                  privacy policies and can be managed through your GitHub
+                  privacy settings). We never post anything to your GitHub
+                  without your permission.
+                </li>
+                <li>
+                  You also have the option to give us more information if you
+                  want to, and this may include “User Personal Information.”
+                </li>
+              </ul>
+            </div>
+          </div>
+          <h2 class="term">
+            Information Disclosure
+          </h2>
+          <div>
+            <p>
+              We do not share, sell, rent, or trade User Personal Information
+              with third parties for commercial purposes. We do share certain
+              aggregated, non-personally identifying information with others
+              about how our users, collectively, use Frontend Social. For
+              example, we may share information pertaining to the popularity of
+              different programming languages for advertising partners.
+            </p>
+            <p>
+              We do host first-party advertising on Frontend Social. We do not
+              run any code from advertisers and all ad images are hosted on
+              managed Frontend Social servers. For more details, see our section
+              on Advertising Details.
+            </p>
+            <p>
+              We may use User Personal Information with your permission, so we
+              can perform services you have authorized. For example, if you
+              indicate you are looking for work, we may contact you with
+              opportunities.
+            </p>
+            <p>
+              We may share User Personal Information with a limited number of
+              third party vendors who process it on our behalf to provide or
+              improve our service, and who have agreed to privacy restrictions
+              similar to our own Privacy Statement. Our third party vendors are
+              listed below.
+            </p>
+          </div>
+          <h2 class="term">
+            Advertising Details
+          </h2>
+          <div>
+            We target advertisements based solely upon:
+            <ul>
+              <li>
+                Details of the page where the advertisement is shown, including:
+                <ul>
+                  <li>
+                    The name and keywords associated with the page or article
+                    being viewed
+                    <ul>
+                      <li>
+                        We allow advertisers to target ads to a list of keywords
+                        advertising.
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            We may place ads in:
+            <ul>
+              <li>Sidebars</li>
+              <li>Below articles</li>
+              <li>On search result pages</li>
+              <li>
+                On tag pages
+              </li>
+              <li>Our dedicated Sponsors page</li>
+            </ul>
+            All registered members have the ability to disable advertisements —
+            where reasonable — through their Settings page. For instance, it’s
+            not feasible to disable certain advertisements in the form of
+            recognition posts, site-wide contests, dedicated sponsorship page,
+            etc.
+          </div>
+          <h2 class="term">
+            Third Party Vendors
+          </h2>
+          <div>
+            We may share your account information with third parties in some
+            circumstances, including:
+            <ol type="1">
+              <li>with your consent;</li>
+              <li>
+                to a service provider or partner who meets our data protection
+                standards;
+              </li>
+              <li>
+                for survey or research purposes, after aggregation,
+                anonymization, or pseudonomization;
+              </li>
+              <li>
+                when we have a good faith belief it is required by law, such as
+                pursuant to a subpoena or other legal process;
+              </li>
+              <li>
+                when we have a good faith belief that doing so will help prevent
+                imminent harm to someone.
+              </li>
+            </ol>
+            <div>
+              <h2>Data Storage</h2>
+              Frontend Social uses third-party vendors and hosting partners for
+              hardware, software, networking, storage, and related technology we
+              need to run Frontend Social. By using Frontend Social Services,
+              you authorize Frontend Social to transfer, store, and use your
+              information in the United States, Canada and any other country
+              where we operate. All service providers and third-party vendors
+              are required to meet our data protection standards.
+            </div>
+            <div>
+              <h2>Site monitoring</h2>
+              Frontend Social uses a variety of third-party services to diagnose
+              errors and improve the performance of our site. We aim to minimize
+              the amount of personal information shared, but the information may
+              include your IP address or other identifying information. All
+              service providers and third-party vendors are required to meet our
+              data protection standards.
+            </div>
+            <div>
+              <h2>Payment processing</h2>
+              Frontend Social does not process payments directly — we rely on
+              third-party services such as Stripe, Shopify, and Paypal to
+              receive payments and store any payment information.
+            </div>
+            <div>
+              <h2>Third-Party Embeds</h2>
+              Some of the content that you see displayed on Frontend Social is
+              not hosted by Frontend Social. These “embeds” are hosted by a
+              third-party and embedded in Frontend Social. For example: YouTube
+              videos, Codepens, Twitter tweets, or GitHub code that appear
+              within a Frontend Social post. These files send data to the hosted
+              site just as if you were visiting that site directly (for example,
+              when you load a Frontend Social post page with a YouTube video
+              embedded in it, YouTube receives data about your activity).
+              Frontend Social does not control what data third parties collect
+              in cases like this, or what they will do with it. Third-party
+              embeds on Frontend Social are not covered by this privacy policy;
+              they are covered by the privacy policy of the third-party service.
+              Be mindful when interacting with these services.
+            </div>
+            <div>
+              <h2>Facebook (visitor action pixel)</h2>
+              We use the “visitor action pixels” from Facebook Inc on our
+              website. This allows user behavior to be tracked after they have
+              been redirected to our website by clicking on a Facebook ad. This
+              enables us to measure the effectiveness of Facebook ads. The data
+              collected in this way is anonymous to us, i.e. we do not see the
+              personal data of individual users. However, this data is stored
+              and processed by Facebook, which is why we are informing you,
+              based on our knowledge of the situation. Facebook may link this
+              information to your Facebook account and also use it for its own
+              promotional purposes, in accordance with Facebook’s Data Usage
+              Policy. You can object to the collection of your data by Facebook
+              pixel, or to the use of your data for the purpose of displaying
+              Facebook ads by contacting the following address:
+              https://www.facebook.com/settings?tab=ads.
+            </div>
+            <div>
+              <h2>
+                Tracking & Cookies
+              </h2>
+              We use browser cookies and similar technologies to recognize you
+              when you return to our Services. Third-party vendors may also use
+              cookies for various reasons.
+              <br>
+              Frontend Social uses a specific cookie in order to facilitate the
+              use of Google Universal Analytics for users logged-in to the
+              Applications or the Platforms (“Logged-In User). If you are a
+              Logged-In User, Frontend Social may use your Frontend Social user
+              ID in combination with Google Universal Analytics and Google
+              Analytics to track and analyze the pages of the Services you
+              visit. We do this only to better understand how you use the
+              Website and the other Services, with a view to offering
+              improvements for all Frontend Social users; and to tailor our
+              business and marketing activities accordingly, both generally and
+              specifically to you. Google Analytics cookies do not provide
+              Frontend Social with any Personal Information.You can prevent
+              Google Analytics from recognizing you on return visits to this
+              site by disabling cookies on your browser. <br>You may opt-out
+              of this feature by installing the Google Analytics Opt-out Browser
+              Add-on, by setting your web browser to refuse cookies, or by
+              setting your browser to alert you when cookies are being sent. If
+              you do so, note that some parts of the Site may not function
+              properly.
+            </div>
+            <div>
+              <h2>Data Security</h2>
+              We use encryption (HTTPS/TLS) to protect data transmitted to and
+              from our site. However, no data transmission over the Internet is
+              100% secure, so we can’t guarantee security. You use the Service
+              at your own risk, and you’re responsible for taking reasonable
+              measures to secure your account.
+            </div>
+            <div>
+              <h2>Administrative Emails from Frontend Social</h2>
+              Sometimes we’ll send you emails about your account, service
+              changes or new policies. You can’t opt out of this type of
+              “transactional” email (unless you delete your account). <br>When
+              you interact with a transactional email sent from Frontend Social
+              (such as opening an email or clicking on a particular link in an
+              email), we may receive information about that interaction.
+            </div>
+            <div>
+              <h2>
+                Non-administrative Emails from Frontend Social
+              </h2>
+              Upon creating a Frontend Social account, you will be opted into
+              the Frontend Social Newsletter and other non-administrative email.
+              Your email address and user profile information may be stored by a
+              third-party email provider such as MailChimp or Sendgrid. You can
+              opt out of non-administrative emails such as digests, newsletters,
+              and activity notifications through your account’s “Settings” page
+              and at the link of the footer in any non-administrative email you
+              receive from us.
+              <br>
+              When you interact with a non-administrative email sent from
+              Frontend Social (such as opening an email or clicking on a
+              particular link in an email), we may receive information about
+              that interaction.
+            </div>
+            <div>
+              <h2>
+                Deleting Your Personal Information
+              </h2>
+              You may request deletion of your personal information and account
+              by emailing contact@shoonya.ca.<br>
+              To protect information from accidental or malicious destruction,
+              we may maintain residual copies for a brief time period. But, if
+              you delete your account, your information and content will be
+              unrecoverable after that time.
+            </div>
+            <div>
+              <h2>Data Portability</h2>
+              If you would like to request a copy of your user data, please
+              email contact@shoonya.ca.
+            </div>
+            <div>
+              <h2>Business Transfers</h2>
+              If we are involved in a merger, acquisition, bankruptcy,
+              reorganization or sale of assets such that your information would
+              be transferred or become subject to a different privacy policy,
+              we’ll notify you in advance of any such change.
+            </div>
+            <div>
+              <h2>Changes to this Policy</h2>
+              We reserve the right to revise this Privacy Policy at any time. If
+              we change this Privacy Policy in the future, we will post the
+              revised Privacy Policy and update the “Effective Date,” above, to
+              reflect the date of the changes.
+            </div>
+            <div>
+              <h2>Questions</h2>
+              We welcome feedback about this policy at contact@shoonya.ca.
+            </div>
+          </div>
+        </div>
       </b-col>
     </b-row>
   </b-container>
@@ -15,3 +351,12 @@ export default {
   name: "PrivacyPolicy"
 };
 </script>
+<style scoped lang="scss">
+.terms {
+  margin-bottom: 2rem;
+}
+
+.term {
+  margin-top: 2rem;
+}
+</style>
