ded59353d8bf62807ef88d28e1859a8919fcbba2
Souvik Basu <souvikbasu@gmail.com>
2019-12-19 09:38:51 -0800

    show upcoming events in home page

---
+++ src/components/Events/UpcomingEvents.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="host">
+    <h1>Upcoming Events</h1>
+    <div class="events">
+      <EventStrip
+        v-for="(event, index) in events"
+        :key="index"
+        :event="event"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import eventService from "@/services/event.service";
+import EventStrip from "@/components/Events/EventStrip";
+
+export default {
+  components: { EventStrip },
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    eventService.getUpcomingEvents().then(events => {
+      this.events = events;
+    });
+  }
+};
+</script>
+
+<style scoped>
+.host {
+  width: 100%;
+}
+
+.events {
+  margin: 20px 10px;
+  text-align: left;
+  width: 100%;
+}
+
+.courtesy {
+  font-size: 0.75rem;
+  text-align: right;
+}
+</style>
