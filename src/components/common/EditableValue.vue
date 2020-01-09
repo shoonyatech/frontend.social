4dd02390b8c4567da841930762256d4660ae1bf3
Souvik Basu <souvikbasu@gmail.com>
2020-01-08 18:32:28 -0800

    add support for multiline in EditavleValue component

--- src/components/common/EditableValue.vue
+++ src/components/common/EditableValue.vue
@@ -1,14 +1,23 @@
 <template>
-  <input
-    v-if="isEditable"
-    :value="value"
-    class="editable-value"
-    :placeholder="placeholder"
-    @change="onChange"
-  >
+  <span v-if="isEditable">
+    <textarea
+      v-if="multiline"
+      :value="value"
+      class="editable-value"
+      :placeholder="placeholder"
+      @change="onChange"
+    />
+    <input
+      v-else
+      :value="value"
+      class="editable-value"
+      :placeholder="placeholder"
+      @change="onChange"
+    >
+  </span>
   <span
     v-else
-    class="value"
+    class="readonly-value"
   >{{ value }}</span>
 </template>
 
@@ -16,6 +25,10 @@
 export default {
   components: {},
   props: {
+    multiline: {
+      type: Boolean,
+      default: false
+    },
     value: {
       type: [String, Number],
       default: ""
@@ -46,7 +59,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.value {
+.readonly-value {
   border-bottom: 1px solid #aada20;
   flex: 1 1 auto;
   margin: 2px auto;
@@ -67,5 +80,9 @@ export default {
   line-height: inherit;
   height: 1.5rem;
   padding: 0 10px;
+}
+
+textarea {
+  min-height: 5rem;
 }
 </style>
