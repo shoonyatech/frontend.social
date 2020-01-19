d1f92fed73eaeeb93879d2b6dfd261c8c9a8a0aa
Souvik Basu <souvikbasu@gmail.com>
2020-01-18 20:52:37 -0800

    make event strip responsive and move link icons to row below

--- src/components/Events/EventStrip.vue
+++ src/components/Events/EventStrip.vue
@@ -1,64 +1,78 @@
 <template>
   <div class="event-strip">
-    <a
-      v-if="!isReadOnly"
-      :href="event.website"
-      target="_blank"
-    >
-      <span>{{ event.title }}</span>
-      <span class="event-type">
-        {{ event.type === "c" ? "conference" : "meetup" }}</span>
-    </a>
-    <div v-else>
-      <span>{{ event.title }}</span>
-      <span class="event-type">
-        {{ event.type === "c" ? "conference" : "meetup" }}</span>
-    </div>
-    <div class="event-date">
-      <span>{{ event.dateFrom | moment("DD MMM YYYY") }}</span>
-      <span v-if="event.dateTo"> - </span>
-      <span v-if="event.dateTo">{{
-        event.dateTo | moment("DD MMM YYYY")
-      }}</span>
-      in
-      <a
-        :href="'/city/' + event.city + '/' + event.country"
-      ><span class="city">{{ event.city }}, {{ event.country }}</span></a>
-    </div>
-    <div
-      v-if="event.relatedSkills"
-      class="event-skills"
-    >
-      <span
-        v-for="(skill, index) in event.relatedSkills"
-        :key="index"
-      >
-        <a>{{ skill }}</a><span v-if="index != event.relatedSkills.length - 1">, </span>
-      </span>
-    </div>
-    <div class="event-description">
-      {{ event.description }}
-    </div>
-    <div
-      v-if="!isReadOnly"
-      class="links icon-links"
-    >
-      <icon-link
-        v-if="event.website"
-        icon="/images/web.svg"
-        :url="event.website"
-      />
-      <icon-link
-        v-if="event.twitter"
-        icon="/images/twitter.svg"
-        :url="event.twitter"
-      />
-      <icon-link
-        v-if="event.youtube"
-        icon="/images/youtube.svg"
-        :url="event.youtube"
-      />
-    </div>
+    <b-row>
+      <b-col md="12">
+        <a
+          v-if="!isReadOnly"
+          :href="event.website"
+          target="_blank"
+        >
+          <span>{{ event.title }}</span>
+          <span class="event-type">
+            {{ event.type === "c" ? "conference" : "meetup" }}</span>
+        </a>
+        <div v-else>
+          <span>{{ event.title }}</span>
+          <span class="event-type">
+            {{ event.type === "c" ? "conference" : "meetup" }}</span>
+        </div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col md="8">
+        <div class="event-date">
+          <span>{{ event.dateFrom | moment("DD MMM YYYY") }}</span>
+          <span v-if="event.dateTo"> - </span>
+          <span v-if="event.dateTo">{{
+            event.dateTo | moment("DD MMM YYYY")
+          }}</span>
+          in
+          <a
+            :href="'/city/' + event.city + '/' + event.country"
+          ><span class="city">{{ event.city }}, {{ event.country }}</span></a>
+        </div>
+        <div
+          v-if="event.relatedSkills"
+          class="event-skills"
+        >
+          <span
+            v-for="(skill, index) in event.relatedSkills"
+            :key="index"
+          >
+            <a>{{ skill }}</a><span v-if="index != event.relatedSkills.length - 1">, </span>
+          </span>
+        </div>
+      </b-col>
+      <b-col md="4">
+        <div
+          v-if="!isReadOnly"
+          class="links icon-links"
+        >
+          <icon-link
+            v-if="event.youtube"
+            icon="/images/youtube.svg"
+            :url="event.youtube"
+          />
+          <icon-link
+            v-if="event.twitter"
+            icon="/images/twitter.svg"
+            :url="event.twitter"
+          />
+          <icon-link
+            v-if="event.website"
+            icon="/images/web.svg"
+            :url="event.website"
+          />
+        </div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col md="12">
+        <div class="event-description">
+          {{ event.description }}
+        </div>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -119,14 +133,8 @@ export default {
   color: #2c3e50;
 }
 
-.links {
-  position: absolute;
-  right: 10px;
-  top: 20px;
-}
-
 .icon-links {
   display: flex;
-  flex-direction: row;
+  flex-direction: row-reverse;
 }
 </style>
