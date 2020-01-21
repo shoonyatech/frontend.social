4b87ab114354630b12ef1872657a320106a1bb32
Souvik Basu <souvikbasu@gmail.com>
2020-01-20 23:03:54 -0800

    extract skill tags from event strip into a component

---
+++ src/components/Skills/SkillTags.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="skill-tags">
+    <div class="skill">
+      <span
+        v-for="(skill, index) in skills"
+        :key="index"
+      >
+        <a>{{ skill }}</a><span v-if="index != skills.length - 1">, </span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SkillTags",
+  components: {},
+  props: {
+    skills: {
+      type: Array
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.skill-tags {
+  flex: 0 1 auto;
+}
+
+.skill {
+  font-size: 0.65rem;
+  color: #2c3e50;
+}
+</style>
