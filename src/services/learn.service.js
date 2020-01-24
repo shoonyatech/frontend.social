161012523bc2751e74aaecd722850fce81dd7632
Souvik Basu <souvikbasu@gmail.com>
2020-01-23 19:30:25 -0800

    set default value of skill in upcoming events and latest articles

--- src/services/learn.service.js
+++ src/services/learn.service.js
@@ -1,7 +1,7 @@
 import httpClient from "./http-client";
 
 export default {
-  getLatestArticles: () => {
+  getLatestArticles: skill => {
     return skill
       ? httpClient.get("article?skill=" + skill)
       : httpClient.get("article");
