9ec6f782bd4815f01ea1c7665ab0f90dfcf65bae
Souvik Basu <souvikbasu@gmail.com>
2020-01-24 00:21:26 -0800

    separate out skill details from skills page

---
+++ src/pages/SkillDetails.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="skill">
+    <b-container>
+      <b-row>
+        <b-col md="12">
+          <h1>
+            <span>{{ title() }}</span>
+          </h1>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="12">
+          <UpcomingEvents :skill="skill" />
+          <LatestArticles :skill="skill" />
+          <div>Jobs</div>
+          <div>Devs</div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import capitalize from "capitalize";
+import learnService from "@/services/learn.service";
+import eventService from "@/services/event.service";
+import jobService from "@/services/job.service";
+import userService from "@/services/user.service";
+
+import LatestArticles from "@/components/Learn/LatestArticles";
+import UpcomingEvents from "@/components/Events/UpcomingEvents";
+
+export default {
+  name: "Skills",
+  components: { LatestArticles, UpcomingEvents },
+  data() {
+    return {
+      skill: null,
+      events: [],
+      jobs: [],
+      articles: [],
+      users: []
+    };
+  },
+  created() {
+    this.skill = this.$route.params.skill;
+
+    eventService.searchEventsBy(`skills=${this.skill}`).then(events => {
+      this.events = events;
+    });
+  },
+  methods: {
+    title() {
+      return capitalize.words(this.skill);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.host {
+  width: 100%;
+}
+
+.skills {
+  margin: 20px 10px;
+  text-align: left;
+  width: 100%;
+}
+</style>
