eca0a7fe30272f1768b671b5e09bbc9965ec6a98
Souvik Basu <souvikbasu@gmail.com>
2020-01-23 18:47:16 -0800

    add skills menu

--- src/App.vue
+++ src/App.vue
@@ -43,6 +43,11 @@ export default {
         text: "City",
         path: "/city",
         icon: "ion-ios-business"
+      },
+      {
+        text: "Skills",
+        path: "/skills",
+        icon: "ion-ios-skills"
       }
     ],
     copyrightText:
