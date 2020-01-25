eb3e49b571602d636ab09db4860b1e21bf708604
Souvik Basu <souvikbasu@gmail.com>
2020-01-25 13:34:42 -0800

    fetch only 10 articles in latest articles

--- src/services/learn.service.js
+++ src/services/learn.service.js
@@ -3,7 +3,7 @@ import httpClient from "./http-client";
 export default {
   getLatestArticles: skill => {
     return skill
-      ? httpClient.get("article?skill=" + skill)
-      : httpClient.get("article");
+      ? httpClient.get("article?count=10&skill=" + skill)
+      : httpClient.get("article?count=10");
   }
 };
