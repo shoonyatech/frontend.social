58d43dc0ab57ed574d3b08bf8335f1f9a74ccf6a
Souvik Basu <souvikbasu@gmail.com>
2020-01-24 20:48:30 -0800

    use framework stat component to compare frameworks

---
+++ src/components/Skills/FrameworkStats.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="host">
+    <div class="framework">
+      {{ framework.name }}
+    </div>
+    <img
+      class="logo"
+      :src="`/images/frameworks/${framework.name}.png`"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FrameworkStats",
+  components: {},
+  props: {
+    framework: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  created() {}
+};
+</script>
+
+<style scoped lang="scss">
+.host {
+  width: 100%;
+}
+
+.logo {
+  height: 50px;
+}
+</style>
