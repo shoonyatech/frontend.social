6cb4efd7967a3d2b27429b2c1b5025a2a6aee8a1
Sanjay Singh <ssingh@falabella.cl>
2020-02-02 15:05:20 +0530

    add arrow component

---
+++ src/components/Arrow/Arrow.vue
@@ -0,0 +1,40 @@
+<template>
+  <img
+    class="arrow"
+    :src="`/images/${imgSrc}.svg`"
+    @click="handleClick"
+  >
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    isExpanded: {
+      type: Boolean,
+      default: false
+    },
+    onClick: {
+      type: Function,
+      default: () => {}
+    }
+  },
+  computed: {
+    imgSrc() {
+      return this.isExpanded ? "up-arrow" : "down-arrow";
+    },
+    handleClick() {
+      return this.onClick;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.arrow {
+  width: 15px;
+  display: flex;
+  cursor: pointer;
+  color: #aada20;
+}
+</style>
