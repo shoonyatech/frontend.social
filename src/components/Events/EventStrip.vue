3f6da4a3b1febfc9560034cb6e97e03ce58178a5
Souvik Basu <souvikbasu@gmail.com>
2020-02-02 21:28:58 -0800

    use Arrow component to show expanded and collapse view of EventStrip

--- src/components/Events/EventStrip.vue
+++ src/components/Events/EventStrip.vue
@@ -64,8 +64,12 @@
     <b-row>
       <b-col md="12">
         <div
+          ref="description"
           class="event-description"
-          :class="{ fullHeight: showMore, lessHeight: !showMore }"
+          :class="{
+            expanded: isExpanded,
+            collapsed: isOverflow
+          }"
         >
           {{ event.description }}
         </div>
@@ -73,21 +77,14 @@
     </b-row>
     <b-row>
       <b-col
+        v-if="showArrow"
         md="12"
         class="arrow-container"
       >
-        <img
-          v-if="!showMore"
-          class="arrow"
-          :src="'/images/down-arrow.svg'"
-          @click="showMore = true"
-        >
-        <img
-          v-if="showMore"
-          class="arrow"
-          :src="'/images/up-arrow.svg'"
-          @click="showMore = false"
-        >
+        <Arrow
+          :is-expanded="isExpanded"
+          :on-click="toggleArrow"
+        />
       </b-col>
     </b-row>
   </div>
@@ -96,12 +93,14 @@
 <script>
 import IconLink from "@/components/common/IconLink";
 import SkillTags from "@/components/Skills/SkillTags";
+import Arrow from "../Arrow/Arrow";
 
 export default {
   name: "EventStrip",
   components: {
     IconLink,
-    SkillTags
+    SkillTags,
+    Arrow
   },
   props: {
     event: {
@@ -113,8 +112,25 @@ export default {
   },
   data() {
     return {
-      showMore: false
+      isExpanded: false,
+      isOverflow: false,
+      showArrow: false
     };
+  },
+  mounted() {
+    var element = this.$refs.description;
+    if (element) {
+      this.isOverflow =
+        element.offsetHeight < element.scrollHeight ||
+        element.offsetWidth < element.scrollWidth;
+      this.showArrow = this.isOverflow;
+    }
+  },
+  methods: {
+    toggleArrow() {
+      this.isExpanded = !this.isExpanded;
+      this.isOverflow = !this.isOverflow;
+    }
   }
 };
 </script>
@@ -145,7 +161,29 @@ export default {
 
 .event-description {
   font-size: 0.8rem;
+  text-align: start;
+  max-height: 100px;
   overflow: hidden;
+}
+
+.collapsed {
+  -webkit-mask-image: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    from(rgba(0, 0, 0, 1)),
+    to(rgba(0, 0, 0, 0))
+  );
+}
+
+.expanded {
+  max-height: unset;
+}
+
+.arrow-container {
+  display: flex;
+  justify-content: center;
+  align-content: center;
 }
 
 .event-skills {
@@ -161,32 +199,5 @@ export default {
 .icon-links {
   display: flex;
   flex-direction: row-reverse;
-}
-
-.lessHeight {
-  height: 50px;
-  -webkit-mask-image: -webkit-gradient(
-    linear,
-    left top,
-    left bottom,
-    from(rgba(0, 0, 0, 1)),
-    to(rgba(0, 0, 0, 0))
-  );
-}
-
-.fullHeight {
-  height: auto;
-}
-
-.arrow-container {
-  display: flex;
-  justify-content: center;
-}
-
-.arrow {
-  width: 15px;
-  display: flex;
-  cursor: pointer;
-  color: #aada20;
 }
 </style>
