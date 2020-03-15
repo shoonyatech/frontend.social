e673d4d33c31bc9c7352334071d575c7d3d09a10
Srashti Jain <srashti0204@gmail.com>
2020-03-16 01:06:20 +0530

    Navbar padding issue resolved

--- src/components/Header/NavBar.vue
+++ src/components/Header/NavBar.vue
@@ -213,11 +213,12 @@ li.nav-item a.nav-item-link.exact-active.active {
   font-weight: bold;
   background-color: #aada1834;
   text-decoration: none;
-  padding: 10px 10px;
 }
 a.nav-item-link:hover {
   background-color: #aada1834;
   text-decoration: none;
+}
+a.nav-item-link {
   padding: 10px 10px;
 }
 </style>
