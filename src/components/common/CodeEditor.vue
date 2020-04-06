36d7cde0513719c9a725e085cf021879ce75e4f7
Srashti Jain <srashti0204@gmail.com>
2020-04-07 03:49:23 +0530

    Added Video Course Page - (Hardcoded links)

---
+++ src/components/common/CodeEditor.vue
@@ -0,0 +1,23 @@
+<template>
+  <iframe
+    src="https://codesandbox.io/embed/xenodochial-browser-5hhd3?fontsize=14&hidenavigation=1&module=%2Fsrc%2Fcomponents%2FHelloWorld.vue&theme=dark"
+    class="code-iframe"
+    title="vvue-youtube-embed-example"
+    allow="geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb"
+    sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+  />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.code-iframe {
+  width: 100%;
+  height: 400px;
+  border: 0;
+  border-radius: 4px;
+  overflow: hidden;
+}
+</style>
---
+++ src/pages/VideoCourse.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="host">
+    <b-container>
+      <b-row>
+        <b-col
+          md="12"
+          sm="12"
+        >
+          <span class="course-heading">{{ course }} > {{ courseName }}</span>
+          <h1>
+            <span>{{ courseTopic }}</span>
+          </h1>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col
+          md="6"
+          sm="12"
+        >
+          <youtube
+            ref="youtube"
+            :video-id="videoId"
+            width="100%"
+            height="400"
+          />
+          <!-- <button @click="getCurrentTime">Get Current Time</button> -->
+        </b-col>
+        <b-col
+          md="6"
+          sm="12"
+        >
+          <CodeEditor />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import CodeEditor from "@/components/common/CodeEditor";
+export default {
+  name: "VideoCourse",
+  components: {
+    CodeEditor
+  },
+  props: {
+    course: {
+      type: String,
+      default: "Angular"
+    },
+    courseName: {
+      type: String,
+      default: "Basic Angular"
+    },
+    courseTopic: {
+      type: String,
+      default: "Data Binding"
+    }
+  },
+  data() {
+    return { videoId: "7iUqMA2Y6xA" };
+  },
+  methods: {
+    getCurrentTime() {
+      this.$refs.youtube.player.getCurrentTime().then(function(value) {
+        alert("Current Time (in seconds): " + value);
+      });
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.host {
+  width: 100%;
+  margin-bottom: 0.8rem;
+}
+.course-heading {
+  color: #4ccccc;
+  font-size: 17px;
+}
+h1 {
+  font-size: 0.9rem;
+}
+</style>
