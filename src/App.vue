624c760ce9d35707fb7b5f8f7ed3e0c7c1858d2f
Souvik Basu <souvikbasu@gmail.com>
2020-05-01 23:26:07 -0700

    remove constraint for sign in in app page

--- src/App.vue
+++ src/App.vue
@@ -60,9 +60,9 @@ export default {
         .getLoggedInUserProfile()
         .then(user => {
           this.$store.commit("signInUser", user);
-          if (user && (user.city == null || user.city === "")) {
-            this.$router.push({ path: "me", query: { msg: "set-city" } });
-          }
+          // if (user && (user.city == null || user.city === "")) {
+          //   this.$router.push({ path: "me", query: { msg: "set-city" } });
+          // }
         })
         .catch(e => {});
     }
