e1d2f912f5495f740e4fa6eb6bb8d7c7d1c3d71e
Mayank Jain <mayank.jain@factset.com>
2020-05-14 22:09:55 +0530

    Feat(Comic strip) Add Comic strip to Home page

---
+++ src/components/ComicStrips/ComicStrips.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-carousel
+      fade
+      indicators
+      img-width="800"
+      img-height="1000"
+    >
+      <b-carousel-slide
+        v-for="(image, index) in images"
+        :key="index"
+        :img-src="image"
+      />
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ComicStrips',
+  data() {
+    return {
+      images: [
+        '/images/comics/Page-01.jpg',
+        '/images/comics/Page-02.jpg',
+        '/images/comics/Page-03.jpg',
+        '/images/comics/Page-04.jpg',]
+    }
+  }
+};
+</script>
+<style scoped lang="scss">
+
+</style>
