08508fccaf2cb56e2dba7d8c72275ccb00399c6b
Mayank Jain <mayank.jain@factset.com>
2020-05-14 22:37:49 +0530

    Added border around comics and on on click open twitter

--- src/components/ComicStrips/ComicStrips.vue
+++ src/components/ComicStrips/ComicStrips.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="comic-box">
+    <span class="label">Frontend City Comic</span>
     <b-carousel
       fade
       indicators
@@ -10,6 +11,7 @@
         v-for="(image, index) in images"
         :key="index"
         :img-src="image"
+        @click.native="onClick"
       />
     </b-carousel>
   </div>
@@ -27,9 +29,30 @@ export default {
         '/images/comics/Page-03.jpg',
         '/images/comics/Page-04.jpg',]
     }
+  },
+  methods: {
+    onClick() {
+      window.open('https://twitter.com/FrontendCity', '_blank')
+    }
   }
 };
 </script>
 <style scoped lang="scss">
+.comic-box {
+  border: 2px solid #114273;
+  margin: 0 auto;
+  padding-bottom: 5px;
+  text-align: center;
+  margin-top: 10px;
+}
 
+.label {
+  background: #114273;
+  color: #fff;
+  border: none;
+  padding: 4px;
+  margin-bottom: 12px;
+  width: 100%;
+  display: inline-block;
+}
 </style>
