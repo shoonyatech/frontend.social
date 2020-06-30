032bbc78998b62bc6f06f7cc5902c98480324c90
Mayank Jain <mayank.jain@factset.com>
2020-07-01 00:35:44 +0530

    feat(tweets) create add delete list page for tweet

---
+++ src/components/tips/TipStrip.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="tip">
+    <div class="tip-title">
+      <div class="info-section">
+        <span
+          v-if="canModify"
+          class="event-action"
+          @click.prevent="editTip"
+        >
+          <img
+            :src="`/images/edit.svg`"
+            class="icon-button"
+            alt="edit"
+          >
+        </span>
+        <span
+          v-if="canModify"
+          class="event-action"
+          @click.prevent="deleteTip"
+        >
+          <img
+            :src="`/images/delete.svg`"
+            class="icon-button"
+            alt="delete"
+          >
+        </span>
+      </div>
+    </div>
+    <Tweet :id="tweetId" />
+  </div>
+</template>
+
+<script>
+import {Tweet} from 'vue-tweet-embed';
+
+export default {
+  name: "TipStrip",
+  components: {
+    Tweet
+  },
+  props: {
+    tip: {
+      type: Object,
+      required: true,
+    }
+  },
+  computed: {
+    canModify() {
+      return this.$store.getters.isAdmin;
+    },
+    tweetId() {
+      return this.tip.twitterLink.split('/status/')[1];
+    }
+  },
+  methods: {
+    deleteTip() {
+      this.$emit('delete', this.tip._id);
+    },
+    editTip() {
+      this.$router.push(`/tip/form/${this.tip._id}`)
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.tip {
+  border-bottom: dotted 1px #114273;
+  padding: 10px;
+  position: relative;
+  min-width: 500px;
+}
+
+.tip-title {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
