6759af03c142c82942046537acd912140262f68a
Souvik Basu <souvikbasu@gmail.com>
2020-09-13 11:32:31 +0530

    feat(challenge): show message on answer submit

--- src/components/Challenge/AddSubmission.vue
+++ src/components/Challenge/AddSubmission.vue
@@ -1,72 +1,86 @@
 <template>
-  <div>
-    <div class="submission-box-container">
-      <div class="submission-box">
-        <EditableValue
-          ref="submissionBox"
-          v-model="submission"
-          :index="1"
-          :is-editable="true"
-          class="value"
-          placeholder="Paste your solution here..."
-          :multiline="true"
-        />
-      </div>
-    </div>
-    <div class="action-buttons">
-      <button
-        class="save-button"
-        @click="save"
-      >
-        Save
-      </button>
-      <!-- <button @click="cancel">
+	<div>
+		<div class="submission-box-container">
+			<div class="submission-box">
+				<EditableValue
+					ref="submissionBox"
+					v-model="submission"
+					:index="1"
+					:is-editable="true"
+					class="value"
+					placeholder="Paste your solution here..."
+					:multiline="true"
+				/>
+			</div>
+		</div>
+		<div class="action-buttons">
+			<button class="save-button" @click="save">
+				Save
+			</button>
+			<!-- <button @click="cancel">
         Cancel
       </button> -->
-    </div>
-  </div>
+		</div>
+	</div>
 </template>
 <script>
-import eventBus from "@/utilities/eventBus";
-import EditableValue from "@/components/common/EditableValue";
-import challengeService from "@/services/challenges.service";
+import eventBus from '@/utilities/eventBus';
+import EditableValue from '@/components/common/EditableValue';
+import challengeService from '@/services/challenges.service';
 
 export default {
-  name: "AddSubmission",
-  components: {
-    EditableValue
-  },
-  props: {
-    challengeId: {
-      type: String,
-      required: true
-    }
-  },
-  data() {
-    return {
-      submission: ''
-    };
-  },
-  
-  methods: {
-    save() {
-      if (this.$refs.submissionBox.editedValue) {
-        challengeService.postSubmission({submission: this.$refs.submissionBox.editedValue, challengeId: this.challengeId}).then(() => {
-          this.$emit('save', this.submission);
-          this.reset();
-        });
-      }
-    },
-    reset() {
-      this.$refs.submissionBox.selectItem("");
-      this.submission = "";
-    }
-  }
+	name: 'AddSubmission',
+	components: {
+		EditableValue
+	},
+	props: {
+		challengeId: {
+			type: String,
+			required: true
+		}
+	},
+	data() {
+		return {
+			submission: ''
+		};
+	},
+
+	methods: {
+		save() {
+			if (this.$refs.submissionBox.editedValue) {
+				challengeService
+					.postSubmission({
+						submission: this.$refs.submissionBox.editedValue,
+						challengeId: this.challengeId
+					})
+					.then(() => {
+						this.$emit('save', this.submission);
+						eventBus.$emit('show-toast', {
+							body: messages.challenge.challengeSubmissionSuccess,
+							title: messages.generic.success
+						});
+
+						this.reset();
+					})
+					.catch(error => {
+						eventBus.$emit('show-toast', {
+							body: messages.challenge.challengeSubmissionFailure,
+							title: messages.generic.error,
+							type: ToastType.ERROR
+						});
+					});
+			}
+		},
+		reset() {
+			this.$refs.submissionBox.selectItem('');
+			this.submission = '';
+		}
+	}
 };
 </script>
 <style lang="scss" scoped>
 .action-buttons {
-  display: flex;
-  justify-content: flex-end;
+	display: flex;
+	justify-content: flex-end;
 }
 </style>
