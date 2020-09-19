7640c8b34866afe2ad166d6b9e77a1c9d2cba6c1
Souvik Basu <souvikbasu@gmail.com>
2020-09-19 01:09:53 -0700

    feat(quiz): add ability to show one question after the other in quiz run

--- src/pages/QuizRun.vue
+++ src/pages/QuizRun.vue
@@ -3,29 +3,55 @@
     <b-card>
       <h2>Quiz Run ID: {{ runId }}</h2>
       <span>
-        <button @click="click">Start Quiz</button>
+        <button @click="startQuiz">Start Quiz</button>
+        <button @click="nextQuestion">Next Question</button>
       </span>
     </b-card>
+    <b-container>
+      <b-row>
+        <b-col md="12">
+          <div
+            v-for="(question, index) in quiz.questions"
+            :key="index"
+          >
+            <QuizQuestion
+              v-if="index === currentQuestion"
+              :question="question"
+              :quiz-id="quiz._id"
+            />
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
+import QuizQuestion from '@/components/Quiz/QuizQuestion';
 import quizService from '@/services/quiz.service';
 export default {
 	name: 'QuestionStrip',
-	components: {},
+	components: { QuizQuestion },
 	props: {},
 	data() {
 		return {
 			runId: 0,
+			quiz: {},
+			currentQuestion: 0,
 		};
 	},
 	mounted() {
 		this.runId = this.$route.params.runId;
+		quizService.getQuizById(this.$route.params.id).then((res) => {
+			this.quiz = res;
+		});
 	},
 	methods: {
-		click() {
+		startQuiz() {
 			this.$router.push(`/quiz/${this.$route.params.id}/run/details`);
+		},
+		nextQuestion() {
+			this.currentQuestion++;
 		},
 	},
 };
