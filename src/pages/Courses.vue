098e21080886d61e3ad25982b775010055e864a1
Souvik Basu <souvikbasu@gmail.com>
2020-09-27 20:55:54 -0700

    fix(course): chapter collapse not working

--- src/pages/Courses.vue
+++ src/pages/Courses.vue
@@ -12,16 +12,7 @@
             <span
               v-if="!infiniteScroll"
               class="navigation-button"
-            >
-              <button
-                :disabled="courses.length === 0"
-                @click="loadCourses('','next')"
-              >&#8250;</button>
-              <button
-                :disabled="page === 1"
-                @click="loadCourses('','previous')"
-              >&#8249;</button>
-            </span>
+            />
           </h1>
           <course-strip
             v-for="(course, index) in courses"
@@ -48,125 +39,125 @@
 </template>
 
 <script>
-import Filters from "@/components/Filters/Filters";
-import CourseStrip from "@/components/Course/CourseStrip";
-import courseService from "@/services/course.service";
-import eventBus from "@/utilities/eventBus";
-import { ToastType, messages } from "@/constants/constants";
+import Filters from '@/components/Filters/Filters';
+import CourseStrip from '@/components/Course/CourseStrip';
+import courseService from '@/services/course.service';
+import eventBus from '@/utilities/eventBus';
+import { ToastType, messages } from '@/constants/constants';
 
 export default {
-  name: "Course",
-  components: {
-    Filters,
-    CourseStrip
-  },
-  props: {
-    infiniteScroll: {
-      type: Boolean,
-      default: false
-    },
-    limit: {
-      type: Number,
-      default: 10
-    }
-  },
-  data() {
-    return {
-      courses: [],
-      skills: [],
-      expertiseLevel: [],
-      jobTypes: [],
-      loading: false,
-      page: 1
-    };
-  },
-  computed: {
-    signedInUser() {
-      return this.$store.state.signedInUser;
-    },
-    isDisableInfiniteScroll() {
-      return !this.infiniteScroll || this.busy;
-    }
-  },
-  created() {
-    setTimeout(() => {
-      if (!this.infiniteScroll) {
-        this.loadCourses("");
-      }
-    }, 500);
-  },
-  methods: {
-    onSearchParamsChange(param = "") {
-      this.loading = true;
-      this.loadCourses(param);
-    },
-    loadCourses(param, action) {
-      switch (action) {
-        case "previous":
-          action = -1;
-          break;
-        case "next":
-          action = 1;
-          break;
-        default:
-          action = 0;
-          break;
-      }
+	name: 'Course',
+	components: {
+		Filters,
+		CourseStrip,
+	},
+	props: {
+		infiniteScroll: {
+			type: Boolean,
+			default: false,
+		},
+		limit: {
+			type: Number,
+			default: 10,
+		},
+	},
+	data() {
+		return {
+			courses: [],
+			skills: [],
+			expertiseLevel: [],
+			jobTypes: [],
+			loading: false,
+			page: 1,
+		};
+	},
+	computed: {
+		signedInUser() {
+			return this.$store.state.signedInUser;
+		},
+		isDisableInfiniteScroll() {
+			return !this.infiniteScroll || this.busy;
+		},
+	},
+	created() {
+		setTimeout(() => {
+			if (!this.infiniteScroll) {
+				this.loadCourses('');
+			}
+		}, 500);
+	},
+	methods: {
+		onSearchParamsChange(param = '') {
+			this.loading = true;
+			this.loadCourses(param);
+		},
+		loadCourses(param, action) {
+			switch (action) {
+				case 'previous':
+					action = -1;
+					break;
+				case 'next':
+					action = 1;
+					break;
+				default:
+					action = 0;
+					break;
+			}
 
-      this.busy = false;
-      this.limit = this.limit || 10;
-      this.page = action + this.page || 1;
+			this.busy = false;
+			this.limit = this.limit || 10;
+			this.page = action + this.page || 1;
 
-      courseService.getCourses(param, this.limit, this.page).then(res => {
-        var courses = res.results;
+			courseService.getCourses(param, this.limit, this.page).then((res) => {
+				var courses = res.results;
 
-        this.skills = res.meta.filters.skills;
+				this.skills = res.meta.filters.skills;
 
-        if (!this.infiniteScroll) {
-          this.courses = courses;
-        } else {
-          this.courses = this.courses.concat(courses);
-          if (courses.length > 0) {
-            ++this.page;
-          }
-        }
-        this.busy = true;
-        this.loading = false;
-      });
-    }
-  }
+				if (!this.infiniteScroll) {
+					this.courses = courses;
+				} else {
+					this.courses = this.courses.concat(courses);
+					if (courses.length > 0) {
+						++this.page;
+					}
+				}
+				this.busy = true;
+				this.loading = false;
+			});
+		},
+	},
 };
 </script>
 
 <style scoped lang="scss">
 .host {
-  width: 100%;
+	width: 100%;
 }
 
 .events {
-  margin: 20px 10px;
-  text-align: left;
-  width: 100%;
+	margin: 20px 10px;
+	text-align: left;
+	width: 100%;
 }
 
 .courtesy {
-  font-size: 0.75rem;
-  text-align: right;
+	font-size: 0.75rem;
+	text-align: right;
 }
 
 .filters-wrapper {
-  height: 100%;
-  border-left: 1px solid #114273;
-  flex-direction: column;
-  display: flex;
-  text-align: start;
-  padding: 10px;
-  cursor: pointer;
+	height: 100%;
+	border-left: 1px solid #114273;
+	flex-direction: column;
+	display: flex;
+	text-align: start;
+	padding: 10px;
+	cursor: pointer;
 }
 
 .course-card {
-  margin: 0.5rem;
-  display: inline-flex;
-  text-align: center;
+	margin: 0.5rem;
+	display: inline-flex;
+	text-align: center;
 }
 </style>
