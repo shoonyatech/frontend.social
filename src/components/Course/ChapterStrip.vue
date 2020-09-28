f10c9d7d0552a2dd3576383ed420369355185ed2
Souvik Basu <souvikbasu@gmail.com>
2020-09-27 20:55:54 -0700

    fix(course): chapter collapse not working

--- src/components/Course/ChapterStrip.vue
+++ src/components/Course/ChapterStrip.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <b-card>
-      <div v-b-toggle="'collapse-' + chapter._id">
-        Chapter {{ chapter.chapterNo }}:{{ chapter.title }}
+      <div v-b-toggle="'collapse-' + chapter.chapterNo">
+        Chapter {{ chapter.chapterNo }}: {{ chapter.title }}
       </div>
       <b-collapse
-        :id="'collapse-' + chapter._id"
+        :id="'collapse-' + chapter.chapterNo"
         class="mt-2"
       >
         <div
@@ -33,90 +33,90 @@
 </template>
 
 <script>
-import {getUrlFriendlyTitle} from '@/utilities/utils';
+import { getUrlFriendlyTitle } from '@/utilities/utils';
 
 export default {
-  name: "ChapterStrip",
-  components: {},
-  props: {
-    chapter: {
-      type: Object,
-      required: true,
-    },
-    courseId: {
-      type: String,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      showSections: false,
-    };
-  },
-  methods: {
-    toggleShowSections() {
-      this.showSections = !this.showSections;
-    },
-    getUrlFriendlyTitle: getUrlFriendlyTitle,
-  },
+	name: 'ChapterStrip',
+	components: {},
+	props: {
+		chapter: {
+			type: Object,
+			required: true,
+		},
+		courseId: {
+			type: String,
+			required: true,
+		},
+	},
+	data() {
+		return {
+			showSections: false,
+		};
+	},
+	methods: {
+		toggleShowSections() {
+			this.showSections = !this.showSections;
+		},
+		getUrlFriendlyTitle: getUrlFriendlyTitle,
+	},
 };
 </script>
 
 <style scoped lang="scss">
 /* style for course thumbnail */
 .thumbnail {
-  border: solid #114273 2px;
-  height: 12rem;
-  width: 12rem;
-  min-width: 12rem;
-  color: #2c3e50;
-  text-decoration: none;
+	border: solid #114273 2px;
+	height: 12rem;
+	width: 12rem;
+	min-width: 12rem;
+	color: #2c3e50;
+	text-decoration: none;
 }
 
 .details {
-  padding: 0.5rem;
-  margin-top: -0.75rem;
-  text-align: left;
+	padding: 0.5rem;
+	margin-top: -0.75rem;
+	text-align: left;
 }
 
 .half-width {
-  width: 50%;
+	width: 50%;
 }
 
 .pr-2 {
-  padding-right: 10px;
+	padding-right: 10px;
 }
 
 .course-title {
-  font-weight: bold;
-  text-align: center;
-  padding-top: 0.8rem;
-  overflow: hidden;
-  white-space: nowrap;
+	font-weight: bold;
+	text-align: center;
+	padding-top: 0.8rem;
+	overflow: hidden;
+	white-space: nowrap;
 }
 .course-technology {
-  font-size: 0.6rem;
-  font-weight: bold;
-  text-align: center;
+	font-size: 0.6rem;
+	font-weight: bold;
+	text-align: center;
 }
 
 .course-description {
-  font-size: 0.6rem;
-  block-size: 6.5rem;
-  overflow: hidden;
+	font-size: 0.6rem;
+	block-size: 6.5rem;
+	overflow: hidden;
 }
 
 .button {
-  text-align: center;
-  position: inherit;
-  padding: 0.5rem;
+	text-align: center;
+	position: inherit;
+	padding: 0.5rem;
 }
 
 .topic-container {
-  border-style: solid;
-  padding: 10px;
-  border-color: #dfdfdf;
-  border-width: 1px;
-  margin-bottom: 1px;
+	border-style: solid;
+	padding: 10px;
+	border-color: #dfdfdf;
+	border-width: 1px;
+	margin-bottom: 1px;
 }
 </style>
