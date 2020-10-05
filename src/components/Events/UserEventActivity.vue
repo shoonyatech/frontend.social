1134cdf44129341d12171f926c0b15e1cbac336f
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-10-05 22:28:43 +0530

    feat(admin): add user profile link on avatar

--- src/components/Events/UserEventActivity.vue
+++ src/components/Events/UserEventActivity.vue
@@ -7,11 +7,15 @@
         </router-link>
       </td>
       <td>
-        <img
-          :src="profileData.profilePic"
-          class="profile-photo"
-          alt="Profile Pic"
+        <a
+          :href="`/user/${profileData.username}`"
+          :title="profileData.username"
         >
+          <img
+            :src="profileData.profilePic"
+            class="profile-photo"
+            alt="Profile Pic"
+          ></a>
       </td>
     </table>
   </div>
