09619da9a367c7c0d1053917a68e5c45a9145f8a
ThPadelis <padelis.theodosiou@gmail.com>
2020-10-05 14:27:03 +0300

    feat: added markers with the user's profile pic

--- src/components/Cities/AvatarMap.vue
+++ src/components/Cities/AvatarMap.vue
@@ -1,53 +1,71 @@
 <template>
-  <b-container>
-    <b-col
-      sm="12"
-      md="12"
-      lg="12"
-      xl="12"
-    >
-      <div
-        id="map"
-        class="avatar-map"
-      />
-    </b-col>
-  </b-container>
+	<b-container>
+		<b-col sm="12" md="12" lg="12" xl="12">
+			<div id="map" class="avatar-map">
+				<div v-if="isLoading" class="avatar-map__loader">Loading...</div>
+			</div>
+		</b-col>
+	</b-container>
 </template>
 
 <script>
 import 'leaflet/dist/leaflet.css';
 import * as L from 'leaflet';
+import * as esri from 'esri-leaflet-geocoder';
+import usersService from '@/services/user.service';
+import { groupBy } from '@/utilities/utils';
+import { Countries } from '@/constants/countries.js';
 
 export default {
 	name: 'AvatarMap',
 	data: () => ({
 		map: null,
-	}),
-
-	mounted() {
-		const baseLayer = L.tileLayer(
+		users: null,
+		isLoading: false,
+		countryCode: null,
+		baseLayer: L.tileLayer(
 			'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 			{
 				attribution:
 					'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 			}
-		);
+		),
+		markers: L.layerGroup(),
+	}),
 
-		(this.map = L.map('map', {
+	async mounted() {
+		this.map = L.map('map', {
 			center: [37.7749, -122.4194],
 			zoom: 13,
-			layers: [baseLayer],
-		})),
-			this.getLocation()
-				.then((position) => {
-					console.log(position);
-					this.map.setView({
-						lon: position.coords.longitude,
-						lat: position.coords.latitude,
-					});
-				})
-				.catch((error) => console.log(error))
-				.finally(() => console.log('Ended'));
+			layers: [this.baseLayer, this.markers],
+		});
+
+		try {
+			const { coords } = await this.getLocation();
+
+			// Sets the center of the map in user's current location
+			this.map.setView({
+				lat: 20.7711443 /* coords.latitude */,
+				lon: 73.7291499 /* coords.longitude */,
+			});
+
+			// Gets the current country code based on user's location
+			esri
+				.geocodeService()
+				.reverse()
+				.latlng([coords.latitude, coords.longitude])
+				.run((error, result) => {
+					if (error) {
+						console.log(error);
+						return;
+					}
+					this.countryCode = 'IN'; // result.address.CountryCode.substring(0, 2);
+				});
+		} catch (error) {
+			console.log('getLocation()', error);
+		}
+
+		this.getUsers();
 	},
 	methods: {
 		getLocation() {
@@ -63,13 +81,71 @@ export default {
 				);
 			});
 		},
+
+		async getUsers() {
+			try {
+				this.users = await usersService.getAllUsers();
+				const notLocatedUsers = this.users.filter((u) => !u.country && !u.city);
+				const locatedUsers = this.users.filter((u) => u.country || u.city);
+
+				for await (let user of locatedUsers) {
+					let countryName = user.country
+						? Countries.find((c) => c.value === user.country).text
+						: null;
+					let city = user.city;
+					let address =
+						countryName && city
+							? `${city}, ${countryName}`
+							: countryName && !city
+							? countryName
+							: !countryName && city
+							? city
+							: '';
+
+					esri
+						.geocode()
+						.text(address)
+						.run((error, result) => {
+							if (error) {
+								console.log('esri.geocode()', error);
+								return;
+							}
+							const latLng = result.results[0].latlng;
+							const icon = L.icon({
+								iconUrl: user.profilePic,
+								iconSize: [25, 25], // size of the icon
+							});
+							const marker = L.marker(latLng, { icon }).addTo(this.markers);
+						});
+				}
+			} catch (error) {
+				console.log('getUsers()', error);
+			}
+		},
 	},
 };
 </script>
 
 <style lang="scss" scoped>
 .avatar-map {
-	height: 300px;
+	height: 450px;
 	border: solid 2px #114273;
+
+	.avatar-map__loader {
+		z-index: 1010;
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+}
+</style>
+
+<style lang="scss">
+.leaflet-marker-icon {
+	border: solid 2px #114273;
+	border-radius: 50%;
 }
 </style>
