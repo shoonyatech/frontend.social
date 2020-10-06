1c4679e979a895009dee0dfef5b6bec2e3833e58
ThPadelis <padelis.theodosiou@gmail.com>
2020-10-06 14:56:47 +0300

    refactor: discard changes on keys.js and config.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,23 +1,23 @@
 export default {
-	google: {
-		clientId:
-			'783965967967-0dbah87uich3o304krbdq70plo9d62nb.apps.googleusercontent.com',
-		clientSecret: 'S_scuM-ol4_mvMryhhkIOCFT',
-	},
 	facebook: {
-		clientId: '917704295293407',
-		clientSecret: 'dd642087523a6ef5531e0c57a4a4a5ec',
+		clientId: '311503802832589',
+		clientSecret: '',
 	},
 	github: {
-		clientId: 'a1cb0b33dc6baa52d49f',
-		clientSecret: '264016e56bf207a4e1b37d94ef812b8d4968cb85',
+		clientId: 'a2c46d9414bce6b94446',
+		clientSecret: '',
 	},
 	twitter: {
-		clientId: '',
+		clientId: 'gtq302zK8hjvczfzbHHJm5ChO',
 		clientSecret: '',
 	},
 	bitbucket: {
 		clientId: '',
+		clientSecret: '',
+	},
+	google: {
+		clientId:
+			'731721117376-ut9ci7uovmv32m7ir4dfuvahb1nf8ap1.apps.googleusercontent.com',
 		clientSecret: '',
 	},
 	linkedin: {
