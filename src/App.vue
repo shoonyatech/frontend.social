74025a35c46c28e42d9bfc99f913ca22f2e466a9
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-11-28 13:34:32 +0530

    feat(navigation bar): redesign navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
-	<div
-		id="app"
-		:class="{ 'theme-dark': isDarkMode, 'theme-light': !isDarkMode }"
-	>
-		<div class="header">
-			<NavBar
-				:nav-links="navLinks"
-				:is-dark-mode="isDarkMode"
-				@toggle-theme="toggleTheme"
-			/>
-			<!-- <button @click="isDarkMode=!isDarkMode">
-        Mode
-      </button> -->
-		</div>
-		<div class="main">
-			<router-view :key="path" />
-		</div>
-		<Footer :copyright-text="copyrightText" />
-		<ToastsManager />
-	</div>
+  <div
+    id="app"
+    :class="{ 'theme-dark': isDarkMode, 'theme-light': !isDarkMode }"
+  >
+    <div class="header">
+      <NavBar
+        :nav-links="navLinks"
+        :is-dark-mode="isDarkMode"
+        :theme="theme"
+        @toggle-theme="toggleTheme"
+      />
+      <!-- <button @click="isDarkMode = !isDarkMode">Mode</button> -->
+    </div>
+    <div class="main">
+      <router-view :key="path" />
+    </div>
+    <Footer :copyright-text="copyrightText" />
+    <ToastsManager />
+  </div>
 </template>
 
 <script>
@@ -28,79 +27,101 @@ import Footer from '@/components/Footer/Footer';
 import ToastsManager from '@/components/ToastsManager/ToastsManager';
 import store from '@/vuex/store';
 export default {
-	components: {
-		NavBar,
-		Footer,
-		ToastsManager,
-	},
-	store,
-	data: () => ({
-		navLinks: [
-			{
-				text: 'Learn',
-				path: '/learn',
-			},
-			{
-				text: 'Jobs',
-				path: '/jobs',
-			},
-			{
-				text: 'Events',
-				path: '/events',
-			},
-			{
-				text: 'City',
-				path: '/city',
-			},
-			{
-				text: 'Tools',
-				path: '/tools',
-			},
-			{
-				text: 'Technologies',
-				path: '/tech',
-			},
-		],
-		isDarkMode: false,
-		copyrightText:
-			'Powered by Shoonya Technologies Ltd. (Canada) © 2020 All Rights Reserved.',
-	}),
-	computed: {
-		path() {
-			return this.$route.fullPath;
-		},
-	},
-	created() {
-		this.getTheme();
-		if (localStorage.getItem('authToken')) {
-			userService
-				.getLoggedInUserProfile()
-				.then((user) => {
-					this.$store.commit('signInUser', user);
-					// if (user && (user.city == null || user.city === "")) {
-					//   this.$router.push({ path: "me", query: { msg: "set-city" } });
-					// }
-				})
-				.catch((e) => {});
-		} else if (localStorage.getItem('GUEST_USER')) {
-			this.$store.commit(
-				'setGuestUser',
-				JSON.parse(localStorage.getItem('GUEST_USER'))
-			);
-		}
+  components: {
+    NavBar,
+    Footer,
+    ToastsManager,
+  },
+  store,
+  data: () => ({
+    theme: 'white-theme',
+    navLinks: [
+      {
+        title: 'Home',
+        href: '/',
+        icon: 'fa fa-home',
+      },
+      {
+        title: 'Learn',
+        href: '/learn',
+        icon: 'fa fa-book',
+      },
+      {
+        title: 'Jobs',
+        href: '/jobs',
+        icon: 'fa fa-briefcase',
+      },
+      {
+        title: 'Events',
+        href: '/events',
+        icon: 'fa fa-bullhorn',
+      },
+      {
+        title: 'City',
+        href: '/city',
+        icon: 'fa fa-map-marker',
+      },
+      {
+        title: 'Tools',
+        href: '/tools',
+        icon: 'fa fa-cog',
+      },
+      {
+        title: 'Technologies',
+        href: '/tech',
+        icon: 'fa fa-desktop',
+      },
+    ],
+    isDarkMode: false,
+    copyrightText:
+      'Powered by Shoonya Technologies Ltd. (Canada) © 2020 All Rights Reserved.',
+  }),
+  computed: {
+    path() {
+      return this.$route.fullPath;
+    },
+  },
+  created() {
+    this.getTheme();
+    if (localStorage.getItem('authToken')) {
+      userService
+        .getLoggedInUserProfile()
+        .then((user) => {
+          this.$store.commit('signInUser', user);
+          // if (user && (user.city == null || user.city === "")) {
+          //   this.$router.push({ path: "me", query: { msg: "set-city" } });
+          // }
+        })
+        .catch((e) => {});
+    } else if (localStorage.getItem('GUEST_USER')) {
+      this.$store.commit(
+        'setGuestUser',
+        JSON.parse(localStorage.getItem('GUEST_USER'))
+      );
+    }
 
-		this.$store.dispatch('fetchSkills');
-	},
-	methods: {
-		getTheme() {
-			const theme = localStorage.getItem('theme');
-			this.isDarkMode = theme === 'DARK';
-		},
-		toggleTheme() {
-			this.isDarkMode = !this.isDarkMode;
-			localStorage.setItem('theme', this.isDarkMode ? 'DARK' : 'LIGHT');
-		},
-	},
+    this.$store.dispatch('fetchSkills');
+  },
+  methods: {
+    getTheme() {
+      const theme = localStorage.getItem('theme');
+      this.isDarkMode = theme === 'DARK';
+      if (this.isDarkMode) {
+        this.theme = '';
+      } else {
+        this.theme = 'white-theme';
+      }
+    },
+    toggleTheme() {
+      this.isDarkMode = !this.isDarkMode;
+      if (this.isDarkMode) {
+        this.theme = '';
+      } else {
+        this.theme = 'white-theme';
+      }
+      localStorage.setItem('theme', this.isDarkMode ? 'DARK' : 'LIGHT');
+    },
+  },
 };
 </script>
 
@@ -108,280 +129,281 @@ export default {
 @import 'https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css';
 
 .theme-dark {
-	--fs-bg: #282c35;
-	--fs-primary-color: #b8daff;
-	--fs-secondary-color: #2693ff; //#114273;
-	--fs-link-color: #74dafc;
+  --fs-bg: #282c35;
+  --fs-primary-color: #b8daff;
+  --fs-secondary-color: #2693ff; //#114273;
+  --fs-link-color: #74dafc;
 }
 
 .theme-light {
-	--fs-bg: #fff;
-	--fs-primary-color: #2c3e50;
-	--fs-secondary-color: #114273; //#114273;
-	--fs-link-color: #1d8db1;
+  --fs-bg: #fff;
+  --fs-primary-color: #2c3e50;
+  --fs-secondary-color: #114273; //#114273;
+  --fs-link-color: #1d8db1;
 }
 
 html {
-	font-size: 22px;
+  font-size: 22px;
 }
 
 body {
-	margin: 0;
-	padding: 0;
-	box-sizing: border-box;
-	font-family: 'Consolas', 'Inconsolata', monospace;
-	font-size: 1rem;
-	font-weight: 400;
-	line-height: 1.5;
-	text-align: left;
-	justify-content: left;
-	overflow-x: hidden;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Consolas', 'Inconsolata', monospace;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  text-align: left;
+  justify-content: left;
+  overflow-x: hidden;
 }
 
 * {
-	-webkit-box-sizing: border-box;
-	box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 #app {
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: left;
-	color: var(--fs-primary-color);
-	background-color: var(--fs-bg);
-	.main {
-		min-height: 83vh;
-		padding-top: 87px;
-		@media screen and (max-width: 759px) {
-			padding-top: 70px;
-		}
-	}
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: var(--fs-primary-color);
+  background-color: var(--fs-bg);
+  .main {
+    min-height: 83vh;
+    padding-top: 87px;
+
+    @media screen and (max-width: 759px) {
+      padding-top: 70px;
+    }
+  }
 }
 
 .header {
-	border-bottom: 7px solid var(--fs-secondary-color);
-	position: fixed;
-	top: 0;
-	z-index: 1200;
-	width: 100%;
-	background-color: white;
-	color: var(--fs-secondary-color);
-	background: var(--fs-bg);
+  border-bottom: 7px solid var(--fs-secondary-color);
+  position: fixed;
+  top: 0;
+  z-index: 1200;
+  width: 100%;
+  background-color: white;
+  color: var(--fs-secondary-color);
+  background: var(--fs-bg);
 }
 
 .icon-button {
-	height: 30px;
-	padding: 5px;
-	cursor: pointer;
+  height: 30px;
+  padding: 5px;
+  cursor: pointer;
 
-	&:hover {
-		background-color: #c0e0ff;
-		border-radius: 9px;
-		border-width: solid transparent 1px;
-	}
+  &:hover {
+    background-color: #c0e0ff;
+    border-radius: 9px;
+    border-width: solid transparent 1px;
+  }
 }
 
 .page {
-	min-height: 920px;
+  min-height: 920px;
 }
 
 .text-sm {
-	font-size: 0.75rem;
+  font-size: 0.75rem;
 }
 
 .text-xs {
-	font-size: 0.6rem;
+  font-size: 0.6rem;
 }
 
 .left {
-	display: inline-block;
-	justify-content: left;
-	text-align: left;
+  display: inline-block;
+  justify-content: left;
+  text-align: left;
 }
 
 .right {
-	display: inline-block;
-	justify-content: right;
-	text-align: right;
+  display: inline-block;
+  justify-content: right;
+  text-align: right;
 }
 
 .inline-block {
-	display: inline-block;
+  display: inline-block;
 }
 
 .page-heading {
-	margin-top: 1rem;
+  margin-top: 1rem;
 }
 
 h1 {
-	font-size: 1.2rem;
-	text-align: left;
-	width: 100%;
-	border-bottom: 1px solid #114273;
-	padding-bottom: 7px;
+  font-size: 1.2rem;
+  text-align: left;
+  width: 100%;
+  border-bottom: 1px solid #114273;
+  padding-bottom: 7px;
 }
 
 h1 button {
-	margin: 2px;
-	float: right;
+  margin: 2px;
+  float: right;
 }
 
 h2 {
-	font-size: 1.1rem;
-	font-weight: 700;
-	text-align: left;
-	width: 100%;
+  font-size: 1.1rem;
+  font-weight: 700;
+  text-align: left;
+  width: 100%;
 }
 
 ol {
-	padding-left: 0.5rem;
-	margin: 0.8rem 0.7rem 0.8rem 1.9em !important;
+  padding-left: 0.5rem;
+  margin: 0.8rem 0.7rem 0.8rem 1.9em !important;
 }
 
 a {
-	color: var(--fs-link-color) !important;
-	cursor: pointer;
+  color: var(--fs-link-color) !important;
+  cursor: pointer;
 }
 
 a:hover {
-	background-color: #11427334;
-	text-decoration: none;
+  background-color: #11427334;
+  text-decoration: none;
 }
 
 label {
-	display: inline-block;
-	color: #114273;
+  display: inline-block;
+  color: #114273;
 }
 
 button {
-	background-color: #114273;
-	color: white;
-	padding: 0 20px;
-	border: 0;
+  background-color: #114273;
+  color: white;
+  padding: 0 20px;
+  border: 0;
 
-	&.error {
-		background-color: rgb(212, 68, 68);
-	}
+  &.error {
+    background-color: rgb(212, 68, 68);
+  }
 }
 
 button:hover {
-	background-color: #38628c;
+  background-color: #38628c;
 
-	&.error {
-		background-color: rgba(212, 68, 68, 0.733);
-	}
+  &.error {
+    background-color: rgba(212, 68, 68, 0.733);
+  }
 }
 
 .light-text {
-	opacity: 0.5;
+  opacity: 0.5;
 }
 
 .radio {
-	display: flex;
+  display: flex;
 }
 
 .radio-input {
-	display: inline-block;
-	height: 29px;
-	margin-right: 10px;
+  display: inline-block;
+  height: 29px;
+  margin-right: 10px;
 }
 
 input {
-	border: 3px solid #114273;
-	display: inline-block;
-	margin: 2px 0;
-	font-size: inherit;
-	line-height: inherit;
-	height: 1.5rem;
-	padding: 0 10px;
+  border: 3px solid #114273;
+  display: inline-block;
+  margin: 2px 0;
+  font-size: inherit;
+  line-height: inherit;
+  height: 1.5rem;
+  padding: 0 10px;
 }
 
 .social-button {
-	font-size: 0.75rem;
-	padding: 5px 10px !important;
-	width: 95%;
-	margin-bottom: 0.25rem;
+  font-size: 0.75rem;
+  padding: 5px 10px !important;
+  width: 95%;
+  margin-bottom: 0.25rem;
 }
 
 .google {
-	background-color: #4285f4;
-	color: #fff;
+  background-color: #4285f4;
+  color: #fff;
 }
 
 .google:hover {
-	background-color: rgba(66, 134, 244, 0.795);
+  background-color: rgba(66, 134, 244, 0.795);
 }
 
 .fb {
-	background-color: #3c5a99;
-	color: #fff;
+  background-color: #3c5a99;
+  color: #fff;
 }
 
 .fb:hover {
-	background-color: #3c5999cc;
+  background-color: #3c5999cc;
 }
 
 .github {
-	background-color: #000;
-	color: #fff;
+  background-color: #000;
+  color: #fff;
 }
 
 .github:hover {
-	background-color: rgba(0, 0, 0, 0.664);
+  background-color: rgba(0, 0, 0, 0.664);
 }
 
 .twitter {
-	background-color: #55acee;
-	color: #fff;
+  background-color: #55acee;
+  color: #fff;
 }
 
 .twitter:hover {
-	background-color: rgba(85, 172, 238, 0.726);
+  background-color: rgba(85, 172, 238, 0.726);
 }
 
 .center-content {
-	width: 100%;
-	text-align: center;
+  width: 100%;
+  text-align: center;
 }
 
 .capsule {
-	color: var(--fs-bg);
-	background-color: var(--fs-primary-color);
-	padding: 0 10px;
-	border-radius: 50px;
-	height: 22px;
-	margin: 0 2px;
-	float: right;
+  color: var(--fs-bg);
+  background-color: var(--fs-primary-color);
+  padding: 0 10px;
+  border-radius: 50px;
+  height: 22px;
+  margin: 0 2px;
+  float: right;
 }
 
 .container {
-	max-width: 1280px;
+  max-width: 1280px;
 }
 
 .full-width {
-	width: 100%;
+  width: 100%;
 }
 
 .breadcrumb {
-	padding: 0;
-	margin: 0;
-	background: transparent;
-	font-size: 18px;
+  padding: 0;
+  margin: 0;
+  background: transparent;
+  font-size: 18px;
 }
 
 .breadcrumb-item + .breadcrumb-item::before {
-	content: '>';
+  content: '>';
 }
 
 .hidden {
-	display: none;
+  display: none;
 }
 
 .sub-text {
-	font-size: 0.65rem;
+  font-size: 0.65rem;
 }
 
 .b-toaster {
-	z-index: 1300;
+  z-index: 1300;
 }
 </style>
--- src/components/Header/NavBar.vue
+++ src/components/Header/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <div class="static-header">
+    <div class="static-header mobile">
       <a
         class="main-logo"
         href="/"
@@ -17,29 +17,34 @@
         :user="signedInUser"
       />
       <div class="menu-icon">
-        <i
-          class="ion-ios-menu"
-          @click="toggleNav"
-        />
+        <div class="toggle">
+          <input
+            id="theme-toggle"
+            type="checkbox"
+            :checked="isDarkMode"
+            @change="$emit('toggle-theme')"
+          >
+          <label for="theme-toggle" />
+        </div>
       </div>
+    </div>
+    <div class="static-header desktop">
+      <a
+        class="main-logo desktop-mage"
+        href="/"
+      >
+        <span class="image-container">
+          <img
+            src="/images/logo.png"
+            alt="Front.Social"
+          >
+        </span>
+      </a>
     </div>
     <ul
       ref="nav"
       class="nav-items"
     >
-      <li
-        v-for="(link, index) in navLinks"
-        :key="index"
-        class="nav-item"
-      >
-        <router-link
-          :to="link.path"
-          class="nav-item-link"
-          @click.native="onClick"
-        >
-          {{ link.text }}
-        </router-link>
-      </li>
       <li
         :key="'signin'"
         class="nav-item right"
@@ -58,7 +63,7 @@
           </div>
         </div>
       </li>
-      <li class="nav-item right">
+      <li class="nav-item">
         <div class="toggle">
           <input
             id="theme-toggle"
@@ -70,226 +75,235 @@
         </div>
       </li>
     </ul>
+    <div class="desktop">
+      <SideNav
+        :menu="navLinks"
+        :is-dark-mode="isDarkMode"
+        :theme="theme"
+      />
+    </div>
+    <div class="mobile">
+      <NavigationTabs :menu="navLinks" />
+    </div>
   </nav>
 </template>
 
 <script>
 import store from '@/vuex/store';
 import Avatar from '@/components/Header/Avatar';
+import SideNav from '@/components/Header/SideNav';
+import NavigationTabs from '@/components/Header/NavigationTabs';
 
 export default {
-	components: { Avatar },
-	props: {
-		navLinks: {
-			type: Array,
-			required: true,
-		},
-		isDarkMode: {
-			type: Boolean,
-			default: false,
-		},
-	},
-	computed: {
-		signedInUser() {
-			return this.$store.state.signedInUser;
-		},
-	},
-	methods: {
-		toggleNav() {
-			const nav = this.$refs.nav.classList;
-			nav.contains('active') ? nav.remove('active') : nav.add('active');
-		},
-		signout() {
-			userService.signout();
-			this.$store.commit('signInUser', null);
-			this.redirect('/');
-		},
-		redirect(path) {
-			this.toggleNav();
-			this.$router.push(path);
-		},
-		onClick() {
-			const nav = this.$refs.nav.classList;
-			nav.remove('active');
-		},
-	},
+  components: { Avatar, SideNav, NavigationTabs },
+  props: {
+    navLinks: {
+      type: Array,
+      required: true,
+    },
+    isDarkMode: {
+      type: Boolean,
+      default: false,
+    },
+    theme: {
+      type: String,
+      default: 'white-theme',
+    },
+  },
+  computed: {
+    signedInUser() {
+      return this.$store.state.signedInUser;
+    },
+  },
+  methods: {
+    signout() {
+      userService.signout();
+      this.$store.commit('signInUser', null);
+      this.redirect('/');
+    },
+    redirect(path) {
+      this.$router.push(path);
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 .static-header {
-	min-width: 200px;
-	display: flex;
-	.menu-icon {
-		display: none;
-	}
-
-	.main-logo {
-		cursor: pointer;
-		display: flex;
-		align-items: center;
-		width: 280px;
-		img {
-			width: 100%;
-		}
-		.image-container {
-			padding: 9px 10px 0 10px;
-			height: 55px;
-		}
-	}
+  min-width: 200px;
+  display: flex;
+  .menu-icon {
+    display: none;
+  }
+  .desktop-mage {
+    margin-left: 50px;
+  }
+  .main-logo {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    width: 280px;
+    img {
+      width: 100%;
+    }
+    .image-container {
+      padding: 9px 10px 0 10px;
+      height: 55px;
+    }
+  }
 }
 
 nav {
-	display: flex;
-	min-height: 55px;
-	width: 100%;
-	.nav-items {
-		display: flex;
-		flex-grow: 0.2;
-		align-items: center;
-		justify-content: space-between;
-		margin: 0;
-		width: 100%;
-		figure {
-			cursor: pointer;
-			margin-right: 10px;
-		}
+  display: flex;
+  min-height: 55px;
+  width: 100%;
+  .nav-items {
+    display: flex;
+    flex-grow: 0.2;
+    align-items: center;
+    justify-content: flex-end;
+    margin: 0;
+    width: 100%;
+    figure {
+      cursor: pointer;
+      margin-right: 10px;
+    }
 
-		i {
-			margin-right: 10px;
-			font-size: 22px;
-		}
-		.nav-item {
-			list-style-type: none;
-			padding: 0 20px;
-			font-size: 24px;
-			cursor: pointer;
-			.nav-item-link {
-				color: var(--fs-secondary-color) !important;
-				display: flex;
-				flex-direction: row-reverse;
-				align-items: center;
-			}
-			&:hover {
-				font-weight: bold;
-			}
-		}
-	}
+    i {
+      margin-right: 10px;
+      font-size: 22px;
+    }
+    .nav-item {
+      list-style-type: none;
+      padding: 0 20px;
+      font-size: 24px;
+      cursor: pointer;
+      .nav-item-link {
+        color: var(--fs-secondary-color) !important;
+        width: 100%;
+        display: flex;
+        flex-direction: row-reverse;
+        align-items: center;
+      }
+      &:hover {
+        font-weight: bold;
+      }
+    }
+  }
 }
 
 .mobile {
-	display: none !important;
+  display: none !important;
 }
 
 @media screen and (max-width: 759px) {
-	.desktop {
-		display: none !important;
-	}
+  .desktop {
+    display: none !important;
+  }
 
-	.mobile {
-		display: flex !important;
-	}
+  .mobile {
+    display: flex !important;
+  }
 
-	.avatar {
-		margin-top: 5px;
-	}
-	.static-header {
-		display: flex;
-		justify-content: space-between;
-		width: 100%;
-		height: 60px;
-		position: fixed;
-		top: 0;
-		z-index: 20;
-		.main-logo {
-			display: flex;
-			align-items: center;
-			width: 245px;
-			padding-left: 10px;
-		}
-		.menu-icon {
-			display: flex;
-			font-size: 45px;
-			padding-right: 30px;
-		}
-	}
-	nav {
-		display: flex;
-		flex-direction: column;
-		.nav-items {
-			display: none;
-		}
-		.nav-items.active {
-			margin: 0;
-			margin-top: 60px;
-			display: flex;
-			align-items: center;
-			padding: 10px;
-			flex-direction: column;
-			font-weight: bold;
-		}
-	}
+  .avatar {
+    margin-top: 5px;
+  }
+  .static-header {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    height: 60px;
+    position: fixed;
+    top: 0;
+    z-index: 20;
+    .main-logo {
+      display: flex;
+      align-items: center;
+      width: 245px;
+      padding-left: 10px;
+    }
+    .menu-icon {
+      display: flex;
+      font-size: 45px;
+      padding-right: 30px;
+    }
+  }
+  nav {
+    display: flex;
+    flex-direction: column;
+    .nav-items {
+      display: none;
+    }
+    .nav-items.active {
+      margin: 0;
+      margin-top: 60px;
+      display: flex;
+      align-items: center;
+      padding: 10px;
+      flex-direction: column;
+      font-weight: bold;
+    }
+  }
 }
 li.nav-item a.nav-item-link.exact-active.active {
-	font-weight: bold;
-	background-color: #11427334;
-	text-decoration: none;
+  font-weight: bold;
+  background-color: #11427334;
+  text-decoration: none;
 }
 a.nav-item-link:hover {
-	background-color: #11427334;
-	text-decoration: none;
+  background-color: #11427334;
+  text-decoration: none;
 }
 a.nav-item-link {
-	padding: 10px 10px;
+  padding: 10px 10px;
 }
 
 .toggle {
-	width: 80px;
-	margin-top: -15px;
+  width: 80px;
+  margin-top: -15px;
 
-	input[type='checkbox'] {
-		display: none;
-	}
+  input[type='checkbox'] {
+    display: none;
+  }
 
-	label {
-		position: relative;
-	}
+  label {
+    position: relative;
+  }
 
-	input[type='checkbox'] + label::before {
-		content: ' ';
-		display: block;
-		height: 26px;
-		width: 45px;
-		border: 1px solid var(--fs-secondary-color);
-		border-radius: 20px;
-		position: absolute;
-		top: 0px;
-		left: -65px;
-		background: white;
-	}
+  input[type='checkbox'] + label::before {
+    content: ' ';
+    display: block;
+    height: 26px;
+    width: 45px;
+    border: 1px solid var(--fs-secondary-color);
+    border-radius: 20px;
+    position: absolute;
+    top: 0px;
+    background: white;
+  }
 
-	input[type='checkbox']:checked + label::before {
-		background: black;
-		transition: all 0.5s ease-in;
-	}
+  input[type='checkbox']:checked + label::before {
+    background: black;
+    transition: all 0.5s ease-in;
+  }
 
-	input[type='checkbox'] + label::after {
-		content: ' ';
-		display: block;
-		height: 20px;
-		width: 20px;
-		border: 1px solid var(--fs-secondary-color);
-		border-radius: 50%;
-		position: absolute;
-		top: 3px;
-		left: -62px;
-		background: var(--fs-secondary-color);
-		transition: all 0.5s ease-in;
-	}
+  input[type='checkbox'] + label::after {
+    content: ' ';
+    display: block;
+    height: 20px;
+    width: 20px;
+    border: 1px solid var(--fs-secondary-color);
+    border-radius: 50%;
+    position: absolute;
+    top: 3px;
+    left: 0px;
+    background: var(--fs-secondary-color);
+    transition: all 0.5s ease-in;
+  }
 
-	input[type='checkbox']:checked + label::after {
-		left: -43px;
-		transition: all 0.5s ease-in;
-	}
+  input[type='checkbox']:checked + label::after {
+    left: 23px;
+    transition: all 0.5s ease-in;
+  }
 }
 </style>
