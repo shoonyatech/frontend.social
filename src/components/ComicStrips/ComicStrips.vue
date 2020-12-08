fc8fb2daa43ab2dff883ae836d2a7e3a543ae43b
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-12-08 13:01:02 +0530

    feat(home): redesign home page

--- src/components/ComicStrips/ComicStrips.vue
+++ src/components/ComicStrips/ComicStrips.vue
@@ -1,84 +1,74 @@
 <template>
   <div class="comic-box">
-    <h1>Frontend City Comic</h1>
-    <b-carousel
-      fade
-      indicators
-      img-width="800"
-      img-height="1000"
-    >
+    <b-carousel fade indicators img-width="800" img-height="1000">
       <b-carousel-slide
         v-for="(image, index) in images"
         :key="index"
         :img-src="image"
       />
     </b-carousel>
-    <div class="twitter-handle">
+    <!-- <div class="twitter-handle">
       <span class="created-by">Created by</span>
-      <a
-        class="link"
-        href="https://twitter.com/FrontendCity"
-        target="_blank"
-      >
-        <img src="/images/twitter.svg">
+      <a class="link" href="https://twitter.com/FrontendCity" target="_blank">
+        <img src="/images/twitter.svg" />
         <span>@FrontendCity</span>
       </a>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
 export default {
-	name: 'ComicStrips',
-	data() {
-		return {
-			images: [
-				'/images/comics/Page-01.jpg',
-				'/images/comics/Page-02.jpg',
-				'/images/comics/Page-03.jpg',
-				'/images/comics/Page-04.jpg',
-				'/images/comics/Page-05.jpg',
-			],
-		};
-	},
-	methods: {
-		onClick() {
-			window.open('https://twitter.com/FrontendCity', '_blank');
-		},
-	},
+  name: 'ComicStrips',
+  data() {
+    return {
+      images: [
+        '/images/comics/Page-01.jpg',
+        '/images/comics/Page-02.jpg',
+        '/images/comics/Page-03.jpg',
+        '/images/comics/Page-04.jpg',
+        '/images/comics/Page-05.jpg',
+      ],
+    };
+  },
+  methods: {
+    onClick() {
+      window.open('https://twitter.com/FrontendCity', '_blank');
+    },
+  },
 };
 </script>
 <style scoped lang="scss">
 .comic-box {
-	margin-top: 70px;
+  margin-top: 20px;
 }
 
 .twitter-handle {
-	margin-top: 30px;
-	margin-right: 10px;
-	display: flex;
-	justify-content: flex-end;
-	img {
-		height: 20px;
-		width: 20px;
-	}
+  margin-top: 30px;
+  margin-right: 10px;
+  display: flex;
+  justify-content: flex-end;
+  img {
+    height: 20px;
+    width: 20px;
+  }
 }
 .link {
-	color: #1d8db1 !important;
-	cursor: pointer;
-	font-size: 0.65rem;
+  color: #1d8db1 !important;
+  cursor: pointer;
+  font-size: 0.65rem;
 }
 .created-by {
-	font-size: 0.65rem;
-	margin-right: 10px;
+  font-size: 0.65rem;
+  margin-right: 10px;
 }
 </style>
 <style lang="scss">
 .carousel-indicators {
-	bottom: -40px !important;
-	li {
-		background-color: black;
-		opacity: 0.2;
-	}
+  bottom: -40px !important;
+  li {
+    background-color: black;
+    opacity: 0.2;
+  }
 }
 </style>
--- src/components/Feedback/Feedback.vue
+++ src/components/Feedback/Feedback.vue
@@ -67,114 +67,114 @@ import eventBus from '@/utilities/eventBus';
 import { ToastType, messages } from '@/constants/constants';
 
 export default {
-	name: 'Feedback',
-	components: {},
-	data() {
-		return {
-			isActive: false,
-			email: '',
-			fullname: '',
-			feedback: '',
-		};
-	},
-	computed: {
-		signedInUser() {
-			return this.$store.state.signedInUser;
-		},
-	},
-	watch: {
-		signedInUser() {
-			this.email = this.signedInUser ? this.signedInUser.email : '';
-			this.fullname = this.signedInUser ? this.signedInUser.name : '';
-		},
-	},
-	mounted() {
-		setTimeout(() => {
-			this.email = this.$store.state.signedInUser
-				? this.$store.state.signedInUser.email
-				: '';
-			this.fullname = this.$store.state.signedInUser
-				? this.$store.state.signedInUser.name
-				: '';
-		}, 1000);
-	},
-	methods: {
-		async sendFeedback() {
-			try {
-				await feedbackService.sendFeedback({
-					name: this.name,
-					email: this.email,
-					feedback: this.feedback,
-				});
-				this.isActive = false;
-				this.feedback = '';
-				eventBus.$emit('show-toast', {
-					body: messages.feedback.success,
-					title: messages.generic.success,
-				});
-			} catch (ex) {
-				eventBus.$emit('show-toast', {
-					body: messages.feedback.error,
-					title: messages.generic.error,
-					type: ToastType.ERROR,
-				});
-			}
-		},
-	},
+  name: 'Feedback',
+  components: {},
+  data() {
+    return {
+      isActive: false,
+      email: '',
+      fullname: '',
+      feedback: '',
+    };
+  },
+  computed: {
+    signedInUser() {
+      return this.$store.state.signedInUser;
+    },
+  },
+  watch: {
+    signedInUser() {
+      this.email = this.signedInUser ? this.signedInUser.email : '';
+      this.fullname = this.signedInUser ? this.signedInUser.name : '';
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.email = this.$store.state.signedInUser
+        ? this.$store.state.signedInUser.email
+        : '';
+      this.fullname = this.$store.state.signedInUser
+        ? this.$store.state.signedInUser.name
+        : '';
+    }, 1000);
+  },
+  methods: {
+    async sendFeedback() {
+      try {
+        await feedbackService.sendFeedback({
+          name: this.name,
+          email: this.email,
+          feedback: this.feedback,
+        });
+        this.isActive = false;
+        this.feedback = '';
+        eventBus.$emit('show-toast', {
+          body: messages.feedback.success,
+          title: messages.generic.success,
+        });
+      } catch (ex) {
+        eventBus.$emit('show-toast', {
+          body: messages.feedback.error,
+          title: messages.generic.error,
+          type: ToastType.ERROR,
+        });
+      }
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 .feedback-box {
-	font-size: 19px;
-	font-size: 1.1875rem;
-	position: fixed;
-	top: 279px;
-	right: -440px;
-	width: 440px;
-	transition: right 0.25s;
-	z-index: 100;
+  font-size: 19px;
+  font-size: 1.1875rem;
+  position: fixed;
+  top: 279px;
+  right: -440px;
+  width: 440px;
+  transition: right 0.25s;
+  z-index: 100;
 
-	&.active {
-		right: -20px;
-	}
+  &.active {
+    right: -20px;
+  }
 }
 
 .feedback-button {
-	background-color: var(--fs-secondary-color);
-	width: 200px;
-	transform: rotate(-90deg);
-	transform-origin: left;
-	color: #fff;
-	padding: 4px 13px;
-	cursor: pointer;
-	margin-bottom: -250px;
-	margin-left: -21px;
-	box-shadow: 0 -2px 5px grey;
-	font-size: 22px;
-	display: flex;
-	justify-content: center;
+  background-color: var(--fs-secondary-color);
+  width: 200px;
+  transform: rotate(-90deg);
+  transform-origin: left;
+  color: #fff;
+  padding: 4px 13px;
+  cursor: pointer;
+  margin-bottom: -250px;
+  margin-left: -21px;
+  box-shadow: 0 -2px 5px grey;
+  font-size: 22px;
+  display: flex;
+  justify-content: center;
 }
 
 .feedback-panel {
-	width: 427px;
-	background: white;
-	padding: 16px;
-	box-shadow: 0 1px 10px grey;
+  width: 427px;
+  background: white;
+  padding: 16px;
+  box-shadow: 0 1px 10px grey;
 
-	input,
-	textarea {
-		width: 100%;
-		margin-bottom: 10px;
-		font-size: 20px;
-	}
+  input,
+  textarea {
+    width: 100%;
+    margin-bottom: 10px;
+    font-size: 20px;
+  }
 
-	button:disabled {
-		opacity: 0.5;
-	}
+  button:disabled {
+    opacity: 0.5;
+  }
 }
 
 .cancel-button {
-	margin-left: 10px;
+  margin-left: 10px;
 }
 </style>
